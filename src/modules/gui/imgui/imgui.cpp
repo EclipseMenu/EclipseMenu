25c7065829d4fddb1d5aa228379d5edc9cba5af9
NDrLEUxcwQsgKUWlrFELocR1z7QP <54683950+maxnut@users.noreply.github.com>
2024-06-22 23:07:06 +0200

    add more gui components

--- src/modules/gui/imgui/imgui.cpp
+++ src/modules/gui/imgui/imgui.cpp
@@ -2,6 +2,7 @@
 
 #include <Geode/Geode.hpp>
 #include <imgui-cocos.hpp>
+#include <misc/cpp/imgui_stdlib.h>
 
 #include <utils.hpp>
 #include <modules/config/config.hpp>
@@ -115,15 +116,61 @@ namespace eclipse::gui::imgui {
             }
         } else if (auto* slider = dynamic_cast<SliderComponent*>(component)) {
             auto value = config::get<float>(slider->getId(), 0.0f);
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x * 0.4f);
             if (ImGui::SliderFloat(slider->getTitle().c_str(), &value, slider->getMin(), slider->getMax(), slider->getFormat().c_str())) {
                 config::set(slider->getId(), value);
                 slider->triggerCallback(value);
             }
+            ImGui::PopItemWidth();
+        } else if (auto* inputfloat = dynamic_cast<InputFloatComponent*>(component)) {
+            auto value = config::get<float>(slider->getId(), 0.0f);
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x * 0.3f);
+            if (ImGui::InputFloat(inputfloat->getTitle().c_str(), &value, 0, 0, inputfloat->getFormat().c_str())) {
+
+                if(value < inputfloat->getMin()) value = inputfloat->getMin();
+                if(value > inputfloat->getMax()) value = inputfloat->getMax();
+
+                config::set(inputfloat->getId(), value);
+                inputfloat->triggerCallback(value);
+            }
+            ImGui::PopItemWidth();
+        } else if (auto* floattoggle = dynamic_cast<FloatToggleComponent*>(component)) {
+            float value = config::get<float>(floattoggle->getId(), 0.0f);
+            bool toggle = config::get<bool>(floattoggle->getId() + ".toggle", false);
+
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x * 0.3f);
+            if (ImGui::InputFloat(("##" + floattoggle->getTitle()).c_str(), &value, 0, 0, floattoggle->getFormat().c_str())) {
+                if(value < floattoggle->getMin()) value = floattoggle->getMin();
+                if(value > floattoggle->getMax()) value = floattoggle->getMax();
+
+                config::set(floattoggle->getId(), value);
+                floattoggle->triggerCallback(value);
+            }
+            ImGui::PopItemWidth();
+
+            ImGui::SameLine();
+            if (ImGui::Checkbox(floattoggle->getTitle().c_str(), &toggle)) {
+                config::set(floattoggle->getId() + ".toggle", toggle);
+                floattoggle->triggerCallback();
+            }
+
         } else if (auto* radio = dynamic_cast<RadioButtonComponent*>(component)) {
             int value = config::get<int>(radio->getId(), radio->getValue());
             if (ImGui::RadioButton(radio->getTitle().c_str(), &value, radio->getValue())) {
                 config::set(radio->getId(), value);
                 radio->triggerCallback(value);
+            }
+        } else if (auto* inputtext = dynamic_cast<InputTextComponent*>(component)) {
+            std::string value = config::get<std::string>(inputtext->getId(), "");
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x * 0.5f);
+            if (ImGui::InputText(inputtext->getTitle().c_str(), &value)) {
+                config::set(inputtext->getId(), value);
+                inputtext->triggerCallback(value);
+            }
+            ImGui::PopItemWidth();
+        } else if (auto* button = dynamic_cast<ButtonComponent*>(component)) {
+            if (ImGui::Button(button->getTitle().c_str())) {
+                button->triggerCallback();
             }
         }
     }
