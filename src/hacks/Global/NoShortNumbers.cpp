aaf831dfd831f245c527a112e5bba636842481bd
zkayns <80561998+zkayns@users.noreply.github.com>
2024-06-24 04:35:08 -0400

    Add files via upload

---
+++ src/hacks/Global/NoShortNumbers.cpp
@@ -0,0 +1,34 @@
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/GameToolbox.hpp>
+
+namespace eclipse::hacks::Global {
+
+    class NoShortNumbers : public hack::Hack {
+        void init() override {
+            auto tab = gui::MenuTab::find("Global");
+            tab->addToggle("No Short Numbers", "global.noshortnumbers");
+        }
+
+        [[nodiscard]] const char* getId() const override { return "No Short Numbers"; }
+    };
+
+    REGISTER_HACK(NoShortNumbers)
+
+    class $modify(GameToolbox) {
+        static void onModify(auto& self) {
+            SAFE_PRIORITY("GameToolbox::intToShortString");
+        }
+
+        static gd::string intToShortString(int value) {
+            if (!config::get<bool>("global.noshortnumbers", false))
+                return GameToolbox::intToShortString(value);
+
+            gd::string str = fmt::format("{}", value);
+            return str;
+        }
+    };
+
+}
---
+++ src/hacks/Global/Speedhack.cpp
@@ -0,0 +1,40 @@
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/CCScheduler.hpp>
+
+namespace eclipse::hacks::Global {
+
+    class Speedhack : public hack::Hack {
+        void init() override {
+            auto tab = gui::MenuTab::find("Global");
+
+            config::setIfEmpty("global.speedhack.toggle", false);
+            config::setIfEmpty("global.speedhack", 1.f);
+
+            tab->addFloatToggle("Speedhack", "global.speedhack", 0.0001f, 1000.f, "%.4f");
+        }
+
+        [[nodiscard]] const char* getId() const override { return "Speedhack"; }
+    };
+
+    REGISTER_HACK(Speedhack)
+
+    class $modify(cocos2d::CCScheduler)
+    {
+        void update(float dt)
+        {
+            float speedhack =
+            config::get<bool>("global.speedhack.toggle", false) ? config::get<float>("global.speedhack", 1.f) : 1.f;
+
+            if(speedhack <= 0)
+                speedhack = 1.f;
+
+            dt *= speedhack;
+
+            cocos2d::CCScheduler::update(dt);
+        }
+    };
+
+}
