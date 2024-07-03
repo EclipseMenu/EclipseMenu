6988df6ea4c1012577b40a45d979ad5788912448
zkayns (AzureBluet) <80561998+zkayns@users.noreply.github.com>
2024-07-03 16:20:05 -0400

    like uhh literally wack

--- src/hacks/Player/Autoclicker.cpp
+++ src/hacks/Player/Autoclicker.cpp
@@ -9,7 +9,11 @@ namespace eclipse::hacks::Player {
     class AutoClicker : public hack::Hack {
         void init() override {
             auto tab = gui::MenuTab::find("Player");
-            tab->addToggle("AutoClicker", "player.autoclick");
+
+            config::setIfEmpty("player.autoclick.toggle", false);
+            config::setIfEmpty("player.autoclick", 1.f);
+
+            tab->addFloatToggle("AutoClicker", "player.autoclick", 0.f, 10.f, "%.1f");
         }
 
         [[nodiscard]] const char* getId() const override { return "AutoClicker"; }
@@ -26,9 +30,10 @@ namespace eclipse::hacks::Player {
         void processCommands(float dt) {
             GJBaseGameLayer::processCommands(dt);
 
-            if (config::get<bool>("player.autoclick", false)) {
+            if (config::get<bool>("player.autoclick.toggle", false)) {
+                float clickInterval = config::get<float>("player.autoclick", 0.f);
                 m_fields->thedt += dt;
-                if (m_fields->thedt > 0.1f) { // FIXME: doesn't click
+                if (m_fields->thedt > clickInterval) { // FIXME: doesn't click
                     m_fields->clicking = !m_fields->clicking;
                     m_fields->clicking ? m_player1->releaseButton(PlayerButton::Jump) : m_player1->pushButton(PlayerButton::Jump);
                     m_fields->thedt = 0.f;
