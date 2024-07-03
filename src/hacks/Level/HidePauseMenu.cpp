ef68bd5bcab4f4f0ba63066cf70ef2056ab6b21f
zkayns (AzureBluet) <80561998+zkayns@users.noreply.github.com>
2024-07-03 16:00:43 -0400

    Update HidePauseMenu.cpp

--- src/hacks/Level/HidePauseMenu.cpp
+++ src/hacks/Level/HidePauseMenu.cpp
@@ -9,7 +9,7 @@ namespace eclipse::hacks::Level {
     class HidePause : public hack::Hack {
         void init() override {
             auto tab = gui::MenuTab::find("Level");
-            tab->addToggle("Hide Pause Menu", "level.hidepause");
+            tab->addToggle("Hide Pause Menu", "level.hidepause")->setDescription("Hides the pause menu. Useful for taking screenshots of levels");
         }
 
         [[nodiscard]] const char* getId() const override { return "Hide Pause Menu"; }
