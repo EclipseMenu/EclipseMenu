df94374106f49abf61e1cf58d76ed38695bf5063
zkayns (AzureBluet) <80561998+zkayns@users.noreply.github.com>
2024-07-05 20:05:57 -0400

    auto kill

---
+++ src/hacks/Player/AutoKill.cpp
@@ -0,0 +1,39 @@
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+
+namespace eclipse::hacks::Player {
+
+    class AutoKill : public hack::Hack {
+        void init() override {
+            auto tab = gui::MenuTab::find("Player");
+
+            config::setIfEmpty("player.autockill.toggle", false);
+            config::setIfEmpty("player.autokill", 50.f);
+
+            tab->addFloatToggle("Auto Kill", "player.autokill", 0.f, 100.f, "%.2f%");
+        }
+
+        [[nodiscard]] const char* getId() const override { return "Auto Kill"; }
+    };
+
+    REGISTER_HACK(AutoKill)
+
+    class $modify(GJBaseGameLayer) {
+        virtual void update(float p0) {
+            auto *playLayer = PlayLayer::get();
+            if (config::get<bool>("player.autokill.toggle", false)) {
+                auto killPercent = config::get<float>("player.autokill");
+                float currentPercent = playLayer->getCurrentPercent();
+                if (currentPercent >= killPercent && playLayer != nullptr) {
+                    playLayer->resetLevel();
+                }
+            }
+            GJBaseGameLayer::update(p0);
+        }
+    };
+
+}
