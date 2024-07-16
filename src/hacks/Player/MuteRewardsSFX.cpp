779ce73800fb79e3ef319ba3797f41312001d753
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-16 16:25:29 -0400

    mute rewards sfx

---
+++ src/hacks/Player/MuteRewardsSFX.cpp
@@ -0,0 +1,40 @@
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/FMODAudioEngine.hpp>
+
+namespace eclipse::hacks::Player {
+
+    class MuteRewardsSFX : public hack::Hack {
+        void init() override {
+            auto tab = gui::MenuTab::find("Player");
+            tab->addToggle("Mute Rewards SFX on Death", "player.muterewardssfx")->setDescription("Disables player death effect")->handleKeybinds();
+        }
+
+        [[nodiscard]] const char* getId() const override { return "Mute Rewards SFX on Death"; }
+    };
+
+    REGISTER_HACK(MuteRewardsSFX)
+
+    class $modify(FMODAudioEngine) {
+        struct Fields {
+            const std::list<std::string> badSFX = { "achievement_01.ogg", "magicExplosion.ogg", "gold02.ogg", "secretKey.ogg" };
+        };
+        static void onModify(auto& self) {
+            SAFE_PRIORITY("FMODAudioEngine::playEffect");
+        }
+
+        void playEffect(gd::string path, float p1, float p2, float p3) {
+            auto pl = PlayLayer::get();
+            if (!pl) { return FMODAudioEngine::sharedEngine()->playEffect(path, p1, p2, p3); } // play sfx if not in playlayer
+            bool isPlayerAlive = !pl->m_player1->m_isDead;
+            bool pauseLayerActive = cocos2d::CCDirector::get()->getRunningScene()->getChildByIDRecursive("PauseLayer");
+            bool notFoundInBadSFX = std::find(m_fields->badSFX.begin(), m_fields->badSFX.end(), std::string(path)) == m_fields->badSFX.end();
+            bool isSettingDisabled = !config::get<bool>("player.muterewardssfx");
+            // these bools could be in one if statement but are separated for readability
+            if (isSettingDisabled || pauseLayerActive || isPlayerAlive || notFoundInBadSFX) { FMODAudioEngine::sharedEngine()->playEffect(path, p1, p2, p3); }
+        }
+    };
+
+}
