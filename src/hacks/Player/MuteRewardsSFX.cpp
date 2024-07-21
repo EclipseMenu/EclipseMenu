a9bc197601f565996a8d9bae42fbf0a70488f32c
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-21 11:30:18 -0400

    forgot desc

--- src/hacks/Player/MuteRewardsSFX.cpp
+++ src/hacks/Player/MuteRewardsSFX.cpp
@@ -10,7 +10,9 @@ namespace eclipse::hacks::Player {
         config::setIfEmpty<bool>("player.muterewardssfx", false);
         void init() override {
             auto tab = gui::MenuTab::find("Player");
-            tab->addToggle("Mute Rewards SFX on Death", "player.muterewardssfx")->setDescription("Disables player death effect")->handleKeybinds();
+            tab->addToggle("Mute Rewards SFX on Death", "player.muterewardssfx")
+                ->setDescription("Disables all rewards SFX (orbs, diamonds, keys) when the player dies. (Created by RayDeeUx)")
+                ->handleKeybinds();
         }
 
         [[nodiscard]] const char* getId() const override { return "Mute Rewards SFX on Death"; }
