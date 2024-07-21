382edfea989e71c2f7d7610e0ccce9304f0bcc51
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-20 20:26:11 -0400

    move to global category

--- src/hacks/Global/CompactViews.cpp
+++ src/hacks/Global/CompactViews.cpp
@@ -5,15 +5,15 @@
 #include <Geode/modify/CustomListView.hpp>
 #include <Geode/modify/LevelCell.hpp>
 
-namespace eclipse::hacks::Cosmetic {
+namespace eclipse::hacks::Global {
 
     class CompactViews : public hack::Hack {
         void init() override {
-            auto tab = gui::MenuTab::find("Cosmetic");
-            tab->addToggle("Compact Editor Levels", "cosmetic.compacteditorlevels")
+            auto tab = gui::MenuTab::find("Global");
+            tab->addToggle("Compact Editor Levels", "global.compacteditorlevels")
                 ->handleKeybinds()
                 ->setDescription("Enables the compact view when browsing custom editor levels. Adapted from code by Cvolton.");
-            tab->addToggle("Compact Profile Comments", "cosmetic.compactprofilecomments")
+            tab->addToggle("Compact Profile Comments", "global.compactprofilecomments")
                 ->handleKeybinds()
                 ->setDescription("Enables the compact view when viewing profile comments. Adapted from code by Cvolton.");
         }
@@ -31,9 +31,9 @@ namespace eclipse::hacks::Cosmetic {
     	and now it finds a new home in eclipsemenu
     	*/
         static CustomListView* create(cocos2d::CCArray* a, TableViewCellDelegate* b, float c, float d, int e, BoomListType f, float g) {
-            if (f == BoomListType::Level2 && config::get<bool>("cosmetic.compacteditorlevels")) {
+            if (f == BoomListType::Level2 && config::get<bool>("global.compacteditorlevels")) {
                 f = BoomListType::Level4; // Level4 = compact level view
-            } else if (f == BoomListType::Comment4 && config::get<bool>("cosmetic.compactprofilecomments")) {
+            } else if (f == BoomListType::Comment4 && config::get<bool>("global.compactprofilecomments")) {
                 f = BoomListType::Comment2; // Comment2 = compact comment view
             }
             return CustomListView::create(a, b, c, d, e, f, g);
@@ -43,7 +43,7 @@ namespace eclipse::hacks::Cosmetic {
 	class $modify(EclipseLevelCell, LevelCell) {
 		void onClick(CCObject* sender) {
 			// hooking this function is necessary in order for the "view" button to work while compact mode is active in "my levels"
-			if (this->m_level->m_levelType == GJLevelType::Editor && config::get<bool>("cosmetic.compacteditorlevels")) {
+			if (this->m_level->m_levelType == GJLevelType::Editor && config::get<bool>("global.compacteditorlevels")) {
 				const auto scene = cocos2d::CCScene::create();
 				scene->addChild(EditLevelLayer::create(m_level));
 				cocos2d::CCDirector::sharedDirector()->replaceScene(cocos2d::CCTransitionFade::create(0.5f, scene));
@@ -51,7 +51,7 @@ namespace eclipse::hacks::Cosmetic {
 		}
 		void loadLocalLevelCell() {
 			LevelCell::loadLocalLevelCell();
-			if (!config::get<bool>("cosmetic.compacteditorlevels")) { return; }
+			if (!config::get<bool>("global.compacteditorlevels")) { return; }
 			if (const auto localLevelname = geode::cast::typeinfo_cast<cocos2d::CCLabelBMFont*>(getChildByIDRecursive("level-name"))) {
 				localLevelname->limitLabelWidth(200.f, .6f, .01f);
 			}
