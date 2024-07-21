a9f3bddb3914a842d8b23d07490af2c083f634de
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-21 13:51:47 -0400

    supply default value anyway

--- src/hacks/Level/MuteLevelSFX.cpp
+++ src/hacks/Level/MuteLevelSFX.cpp
@@ -22,7 +22,7 @@ namespace eclipse::hacks::Level {
 
     class $modify(EffectGameObject) {
         void triggerObject(GJBaseGameLayer* gjbgl, int p1, gd::vector<int> const* p2) {
-            if (!PlayLayer::get() || !config::get<bool>("level.mutelevelsfx")) { return EffectGameObject::triggerObject(gjbgl, p1, p2); }
+            if (!PlayLayer::get() || !config::get<bool>("level.mutelevelsfx", false)) { return EffectGameObject::triggerObject(gjbgl, p1, p2); }
             if (this->m_objectID != 3602 && this->m_objectID != 3603) { return EffectGameObject::triggerObject(gjbgl, p1, p2); }
         }
     };
