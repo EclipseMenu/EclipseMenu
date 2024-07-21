0bd8ba8965565f105f5df699eae7daebaf55d2e1
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-21 10:57:10 -0400

    default values

--- src/hacks/Global/CompactViews.cpp
+++ src/hacks/Global/CompactViews.cpp
@@ -9,13 +9,15 @@ namespace eclipse::hacks::Global {
 
     class CompactViews : public hack::Hack {
         void init() override {
+        	config::setIfEmpty("global.compacteditorlevels", false);
+        	config::setIfEmpty("global.compactprofilecomments", false);
             auto tab = gui::MenuTab::find("Global");
             tab->addToggle("Compact Editor Levels", "global.compacteditorlevels")
                 ->handleKeybinds()
-                ->setDescription("Enables the compact view when browsing custom editor levels. Adapted from code by Cvolton.");
+                ->setDescription("Enables the compact view when browsing custom editor levels. Adapted from code by Cvolton. (Created by RayDeeUx)");
             tab->addToggle("Compact Profile Comments", "global.compactprofilecomments")
                 ->handleKeybinds()
-                ->setDescription("Enables the compact view when viewing profile comments. Adapted from code by Cvolton.");
+                ->setDescription("Enables the compact view when viewing profile comments. Adapted from code by Cvolton. (Created by RayDeeUx)");
         }
 
         [[nodiscard]] const char* getId() const override { return "Compact Views"; }
@@ -29,11 +31,12 @@ namespace eclipse::hacks::Global {
     	and now reused for profile comments yayy
     	proof of consent of code reuse: https://discord.com/channels/911701438269386882/911702535373475870/1220117410988953762
     	and now it finds a new home in eclipsemenu
+    	-- raydeeux
     	*/
         static CustomListView* create(cocos2d::CCArray* a, TableViewCellDelegate* b, float c, float d, int e, BoomListType f, float g) {
-            if (f == BoomListType::Level2 && config::get<bool>("global.compacteditorlevels")) {
+            if (f == BoomListType::Level2 && config::get<bool>("global.compacteditorlevels", true)) {
                 f = BoomListType::Level4; // Level4 = compact level view
-            } else if (f == BoomListType::Comment4 && config::get<bool>("global.compactprofilecomments")) {
+            } else if (f == BoomListType::Comment4 && config::get<bool>("global.compactprofilecomments", true)) {
                 f = BoomListType::Comment2; // Comment2 = compact comment view
             }
             return CustomListView::create(a, b, c, d, e, f, g);
@@ -43,7 +46,7 @@ namespace eclipse::hacks::Global {
 	class $modify(EclipseLevelCell, LevelCell) {
 		void onClick(CCObject* sender) {
 			// hooking this function is necessary in order for the "view" button to work while compact mode is active in "my levels"
-			if (this->m_level->m_levelType == GJLevelType::Editor && config::get<bool>("global.compacteditorlevels")) {
+			if (this->m_level->m_levelType == GJLevelType::Editor && config::get<bool>("global.compacteditorlevels", true)) {
 				const auto scene = cocos2d::CCScene::create();
 				scene->addChild(EditLevelLayer::create(m_level));
 				cocos2d::CCDirector::sharedDirector()->replaceScene(cocos2d::CCTransitionFade::create(0.5f, scene));
@@ -51,12 +54,13 @@ namespace eclipse::hacks::Global {
 		}
 		void loadLocalLevelCell() {
 			LevelCell::loadLocalLevelCell();
-			if (!config::get<bool>("global.compacteditorlevels")) { return; }
-			if (const auto localLevelname = geode::cast::typeinfo_cast<cocos2d::CCLabelBMFont*>(getChildByIDRecursive("level-name"))) {
-				localLevelname->limitLabelWidth(200.f, .6f, .01f);
-			}
-			if (const auto mainLayer = geode::cast::typeinfo_cast<CCLayer*>(getChildByIDRecursive("main-layer"))) {
-				mainLayer->setPositionY(-3.5f);
+			if (config::get<bool>("global.compacteditorlevels", true)) {
+				if (const auto localLevelname = geode::cast::typeinfo_cast<cocos2d::CCLabelBMFont*>(getChildByIDRecursive("level-name"))) {
+					localLevelname->limitLabelWidth(200.f, .6f, .01f);
+				}
+				if (const auto mainLayer = geode::cast::typeinfo_cast<CCLayer*>(getChildByIDRecursive("main-layer"))) {
+					mainLayer->setPositionY(-3.5f);
+				}
 			}
 		}
 	};
