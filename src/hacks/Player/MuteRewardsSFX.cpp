c9d683e527e57a5b9d839a2bd493e6a79e784b70
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-22 21:46:23 -0400

    Five new features (#12)
    * compact views
    
    * bruh.
    
    * @ninxout wake up and find intel bindings goddammit
    
    * mute level sfx
    
    * undo oopsies
    
    * mute rewards sfx
    
    * hide level complete vfx
    
    * move to global category
    
    * no platformer jump squish
    
    * default values
    
    * default values
    
    * default values
    
    * default values
    
    * forgot desc
    
    * forgot desc, add comment
    
    * default values
    
    * rm type
    
    * `setIfEmpty` may or may not need a type and at this rate it's a gamble
    
    * oml
    
    * oml i need better glasses
    
    * optimization
    
    * round two
    
    * round two
    
    * minimize crash
    
    * supply default value anyway
    
    * supply default value anyway
    
    * use tab indents + all config defaults are false
    
    * use array + more optimized hooking per spagh suggestion
    
    * what the fuck dude 😭 // just use std::array // omg // i said it like 5 times now // dont use a raw c array
    
    * remove config::setifempty

---
+++ src/hacks/Player/MuteRewardsSFX.cpp
@@ -0,0 +1,43 @@
+#include <iterator>
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/FMODAudioEngine.hpp>
+
+namespace eclipse::hacks::Player {
+
+	class MuteRewardsSFX : public hack::Hack {
+		void init() override {
+			auto tab = gui::MenuTab::find("Player");
+			tab->addToggle("Mute Rewards SFX on Death", "player.muterewardssfx")
+				->setDescription("Disables all rewards SFX (orbs, diamonds, keys) when the player dies. (Created by RayDeeUx)")
+				->handleKeybinds();
+		}
+
+		[[nodiscard]] const char* getId() const override { return "Mute Rewards SFX on Death"; }
+	};
+
+	REGISTER_HACK(MuteRewardsSFX)
+
+	class $modify(FMODAudioEngine) {
+		struct Fields {
+			const std::array<std::string, 4> badSFX = { "achievement_01.ogg", "magicExplosion.ogg", "gold02.ogg", "secretKey.ogg" };
+		};
+		static void onModify(auto& self) {
+			SAFE_PRIORITY("FMODAudioEngine::playEffect");
+		}
+
+		void playEffect(gd::string path, float p1, float p2, float p3) {
+			auto pl = PlayLayer::get();
+			if (!pl) { return FMODAudioEngine::sharedEngine()->playEffect(path, p1, p2, p3); } // play sfx if not in playlayer
+			bool isPlayerAlive = !pl->m_player1->m_isDead;
+			bool pauseLayerActive = cocos2d::CCDirector::get()->getRunningScene()->getChildByIDRecursive("PauseLayer");
+			bool notFoundInBadSFX = std::ranges::find(std::begin(m_fields->badSFX), std::end(m_fields->badSFX), std::string(path)) == std::end(m_fields->badSFX);
+			bool isSettingDisabled = !config::get<bool>("player.muterewardssfx", false);
+			// these bools could be in one if statement but are separated for readability
+			if (isSettingDisabled || pauseLayerActive || isPlayerAlive || notFoundInBadSFX) { FMODAudioEngine::sharedEngine()->playEffect(path, p1, p2, p3); }
+		}
+	};
+
+}
---
+++ src/hacks/Player/NoPlatformerJumpSquish.cpp
@@ -0,0 +1,29 @@
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/PlayerObject.hpp>
+
+namespace eclipse::hacks::Player {
+
+	class NoPlatformerJumpSquish : public hack::Hack {
+		void init() override {
+			auto tab = gui::MenuTab::find("Player");
+			tab->addToggle("No Platformer Jump Squish", "player.noplatformersquish")
+				->setDescription("Disables the squishing animation when jumping in place while playing platformer levels. (Created by RayDeeUx)")
+				->handleKeybinds();
+		}
+
+		[[nodiscard]] const char* getId() const override { return "No Platformer Jump Squish"; }
+	};
+
+	REGISTER_HACK(NoPlatformerJumpSquish)
+
+	class $modify(MyPlayerObject, PlayerObject){
+		void animatePlatformerJump(float p0) {
+			if (!config::get<bool>("player.noplatformersquish", false))
+				PlayerObject::animatePlatformerJump(p0);
+		}
+	};
+
+}
