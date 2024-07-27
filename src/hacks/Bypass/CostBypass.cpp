56310784448235c9b4915f76aa34eaa5e3ed5dfc
zkayns (AzureBluet) <80561998+zkayns@users.noreply.github.com>
2024-07-27 13:36:28 -0400

    cost bypass

---
+++ src/hacks/Bypass/CostBypass.cpp
@@ -0,0 +1,32 @@
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/GameStatsManager.hpp>
+
+namespace eclipse::hacks::Bypass {
+
+    class CostBypass : public hack::Hack {
+        void init() override {
+            auto tab = gui::MenuTab::find("Bypass");
+
+            tab->addToggle("Cost Bypass", "bypass.costbypass")
+                ->handleKeybinds()
+                ->setDescription("Sets the cost of all shop items to 0 orbs.");
+        }
+
+        [[nodiscard]] const char* getId() const override { return "Cost Bypass"; }
+    };
+
+    REGISTER_HACK(CostBypass)
+
+    class $modify(GameStatsManager) {
+        void addStoreItem(int p0, int p1, int p2, int p3, ShopType p4) {
+            if (config::get<bool>("bypass.costbypass", false)) {
+                GameStatsManager::addStoreItem(p0, p1, p2, 0, p4);
+            } else {
+                GameStatsManager::addStoreItem(p0, p1, p2, p3, p4);
+            };
+        }
+    };
+}
