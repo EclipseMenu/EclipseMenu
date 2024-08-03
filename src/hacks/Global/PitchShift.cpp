3dd1721de4eb3d2971995c5fdac3f8715b26bf83
Oleksandr Nemesh <prevter@gmail.com>
2024-08-04 01:13:20 +0300

    sync audio with speedhack option

--- src/hacks/Global/PitchShift.cpp
+++ src/hacks/Global/PitchShift.cpp
@@ -53,7 +53,7 @@ namespace eclipse::hacks::Global {
         }
 
         [[nodiscard]] const char* getId() const override { return "Pitch Shift"; }
-        [[nodiscard]] int32_t getPriority() const override { return -8; }
+        [[nodiscard]] int32_t getPriority() const override { return -9; }
     };
 
     REGISTER_HACK(PitchShift)
