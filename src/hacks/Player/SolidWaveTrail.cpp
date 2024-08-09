e88395df2a8f2c8457b85c96fa82deb8e0bb045b
Rustring <155897302+Rustring@users.noreply.github.com>
2024-08-09 12:37:08 +0000

    Solid Wave Trail (Final)
    Everything should work as it should

--- src/hacks/Player/SolidWaveTrail.cpp
+++ src/hacks/Player/SolidWaveTrail.cpp
@@ -2,13 +2,15 @@
 #include <modules/hack/hack.hpp>
 #include <modules/config/config.hpp>
 
-#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/Geode.hpp>
 #include <Geode/modify/CCDrawNode.hpp>
 #include <Geode/modify/HardStreak.hpp>
 
+using namespace geode::prelude;
+
 namespace eclipse::hacks::Player {
 
-    class SolidWaveTrail : public hack::Hack {
+    class SolidWaveTrail : public hack::Hack {    // Hack name and desciption
         void init() override {
             auto tab = gui::MenuTab::find("Player");
             tab->addToggle("Solid Wave Trail", "player.solidwavetrail")
@@ -19,14 +21,15 @@ namespace eclipse::hacks::Player {
         [[nodiscard]] const char* getId() const override { return "Solid Wave Trail"; }
     };
 
-    REGISTER_HACK(SolidWaveTrail)
+   REGISTER_HACK(SolidWaveTrail)
 
-   class $modify(cocos2d::CCDrawNode) {    // This code is somewhat broken but this right now, this gives the wave trail a darker color.
+   class $modify(cocos2d::CCDrawNode) {    // The actual hack code
        bool drawPolygon(cocos2d::CCPoint *p0, unsigned int p1, const cocos2d::ccColor4F &p2, float p3, const cocos2d::ccColor4F &p4) {
            if (!config::get<bool>("player.solidwavetrail", true))
              return CCDrawNode::drawPolygon(p0,p1,p2,p3,p4);
            if (p2.r == 1.F && p2.g == 1.F && p2.b == 1.F && p2.a != 1.F) 
              return true; 
+           this->setBlendFunc(CCSprite::create()->getBlendFunc());
            this->setZOrder(-1); 
            return CCDrawNode::drawPolygon(p0,p1,p2,p3,p4);
        }
