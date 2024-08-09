024a76a95fb0cc8fd17756251f672edffd4e26b2
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-08-09 11:03:03 -0400

    rewrite
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/hacks/Level/HideLevelCompleteVFX.cpp
+++ src/hacks/Level/HideLevelCompleteVFX.cpp
@@ -24,54 +24,57 @@ namespace eclipse::hacks::Level {
     class $modify(HideLevelCompleteVFXCCCWHook, CCCircleWave) {
         void draw() {
             CCCircleWave::draw();
+
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return;
+
             PlayLayer* pl = PlayLayer::get();
+
             if (!pl) return;
 
-            if (pl->m_levelEndAnimationStarted && config::get<bool>("level.hidelevelcomplete", false))
-                /*
-                CCCircleWaves sometimes get added
-                as child of CurrencyRewardLayer when completing rated levels
-                so i had to change hooks to CCCircleWave::draw();
+            /*
+            CCCircleWaves sometimes get added
+            as child of CurrencyRewardLayer when completing rated levels
+            so i had to change hooks to CCCircleWave::draw();
 
-                this doesnt affect CCCircleWaves when playlayer is active
-                thanks to the m_levelEndAnimationStarted member variable
-                */
-                    if (!geode::cast::typeinfo_cast<CurrencyRewardLayer*>(this->getParent()))
-                        this->setVisible(false);
+            this doesnt affect CCCircleWaves when playlayer is active
+            thanks to the m_levelEndAnimationStarted member variable
+            */
+            if (pl->m_levelEndAnimationStarted && !geode::cast::typeinfo_cast<CurrencyRewardLayer*>(this->getParent()))
+                this->setVisible(false);
         }
     };
 
     class $modify(HideLevelCompleteVFXCCLFHook, CCLightFlash) {
         // i cant believe i need to hook this function with TWENTY params to get things working what the heck :despair:
         void playEffect(cocos2d::CCPoint point, cocos2d::ccColor3B color, float p2, float p3, float p4, float p5, float p6, float p7, float p8, float p9, float p10, float p11, float p12, float p13, float p14, float p15, int p16, bool p17, bool p18, float p19) {
+            CCLightFlash::playEffect(
+                point, color, p2, p3, p4, p5, p6, p7, p8, p9, p10,
+                p11, p12, p13, p14, p15, p16, p17, p18, p19
+            );
+
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return;
+
             PlayLayer* pl = PlayLayer::get();
 
-            if (!pl)
-                return CCLightFlash::playEffect(
-                    point, color, p2, p3, p4, p5, p6, p7, p8, p9, p10,
-                    p11, p12, p13, p14, p15, p16, p17, p18, p19
-                );
+            if (!pl) return;
 
-            if (pl->m_levelEndAnimationStarted && config::get<bool>("level.hidelevelcomplete", false))
+            if (pl->m_levelEndAnimationStarted)
                 this->setVisible(false);
-
-            CCLightFlash::playEffect(
-                point, color, p2, p3, p4, p5, p6, p7, p8, p9,
-                p10, p11, p12, p13, p14, p15, p16, p17, p18, p19
-            );
         }
     };
 
     // cannot hook the class below due to macos arm/intel differences (?)
+    // nvm it was some setupVBO() issue
     /*
     class $modify(HideLevelCompleteVFXCCPSQHook, cocos2d::CCParticleSystemQuad) {
         static cocos2d::CCParticleSystemQuad* create(char const* p0, bool p1) {
             auto psq = cocos2d::CCParticleSystemQuad::create(p0, p1);
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return psq;
             PlayLayer* pl = PlayLayer::get();
             if (!pl) return psq;
             if (psq->getParent() != pl) return psq;
 
-            if (pl->m_levelEndAnimationStarted && config::get<bool>("level.hidelevelcomplete", false))
+            if (pl->m_levelEndAnimationStarted)
                 psq->setVisible(false);
 
             return psq;
@@ -81,12 +84,16 @@ namespace eclipse::hacks::Level {
     class $modify(HideLevelCompleteVFXCCPSHook, cocos2d::CCParticleSystem) {
         void update(float dt) {
             CCParticleSystem::update(dt);
+
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return;
+
             PlayLayer* pl = PlayLayer::get();
+
             if (!pl) return;
             if (this->getParent() != pl) return;
             if (!geode::cast::typeinfo_cast<cocos2d::CCParticleSystemQuad*>(this)) return;
 
-            if (pl->m_levelEndAnimationStarted && config::get<bool>("level.hidelevelcomplete", false))
+            if (pl->m_levelEndAnimationStarted)
                 this->setVisible(false);
         }
     };
