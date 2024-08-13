d50d9698f76da79d76a8255a8e6e09c55cc3313b
iAndyHD3 <54410739+iAndyHD3@users.noreply.github.com>
2024-08-13 12:03:41 +0200

    add legacy reverse physics (#21)
    * add legacy reverse physics
    
    * fix oopsie

---
+++ src/hacks/Level/LegacyReversePhysics.cpp
@@ -0,0 +1,63 @@
+#include "Geode/binding/GameManager.hpp"
+#include "Geode/binding/LevelSettingsObject.hpp"
+#include "Geode/binding/PlayLayer.hpp"
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+#include <modules/config/config.hpp>
+
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/LevelSettingsObject.hpp>
+
+using namespace cocos2d;
+
+namespace eclipse::hacks::Level {
+
+
+    class $modify(LegacyPhysicsPlayLayer, PlayLayer) {
+        struct Fields {
+            bool originalFixGravityVal = false;
+        };
+
+        void toggleFixGravityBugState(bool newState) {
+            m_levelSettings->m_fixGravityBug = newState ? false : m_fields->originalFixGravityVal; 
+        }
+
+        bool init(GJGameLevel *level, bool useReplay, bool dontCreateObjects) {
+            if(!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+            m_fields->originalFixGravityVal = m_levelSettings->m_fixGravityBug;
+            if(config::get<bool>("level.legacyreversephysics", false)) {
+                m_levelSettings->m_fixGravityBug = false;
+            }
+            return true;
+        }
+    };
+    
+    class LegacyReversePhysics : public hack::Hack {
+        void init() override {
+            auto tab = gui::MenuTab::find("Level");
+
+            tab->addToggle("Legacy Reverse Physics", "level.legacyreversephysics")
+            ->handleKeybinds()
+            ->setDescription("Forces old upside down physics among other old physics changes")
+            ->callback([](bool newState){
+                if(auto pl = GameManager::get()->getPlayLayer()) {
+                    static_cast<LegacyPhysicsPlayLayer*>(pl)->toggleFixGravityBugState(newState);
+                }
+            });
+        }
+
+        //player would be cheating only if level is new physics and legacy physics is active
+        //otherwise the hack being on doesn't matter
+        [[nodiscard]] bool isCheating() override {
+            if(!config::get<bool>("level.legacyreversephysics", false)) return false;
+            auto pl = GameManager::sharedState()->getPlayLayer();
+            if(!pl) return false;
+
+            return static_cast<LegacyPhysicsPlayLayer*>(pl)->m_fields->originalFixGravityVal;
+        }
+
+        [[nodiscard]] const char* getId() const override { return "Legacy Reverse Physics"; }
+    };
+
+    REGISTER_HACK(LegacyReversePhysics)
+}
