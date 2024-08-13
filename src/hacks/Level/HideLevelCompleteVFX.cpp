1c1041ec9bd38bb3a7c02d8677bf0df916909e46
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-08-13 11:14:49 -0400

    Ironing out my features (#18)
    * committing several hooking related war crimes (maybe)
    
    * newlines + ironing out some minor issues
    
    * no manual newlines i guess :pensive:
    
    * more inlining
    
    * fix death sfx not playing
    
    * hide particles + remove getChildByID calls
    
    * add casting check because i can
    
    * rewrite
    
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    
    * change hook priority to avoid ControlVanillaSFX unexpected behavior
    
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    
    * change hook priority to avoid ControlVanillaSFX unexpected behavior
    
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    
    * change hook priority to avoid ControlVanillaSFX unexpected behavior
    
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    
    * dont use update(dt)
    
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/hacks/Level/HideLevelCompleteVFX.cpp
+++ src/hacks/Level/HideLevelCompleteVFX.cpp
@@ -4,15 +4,16 @@
 
 #include <Geode/modify/CCCircleWave.hpp>
 #include <Geode/modify/CCLightFlash.hpp>
+#include <Geode/modify/CCParticleSystemQuad.hpp>
+#include <Geode/modify/CCParticleSystem.hpp>
 
 namespace eclipse::hacks::Level {
-
     class HideLevelCompleteVFX : public hack::Hack {
         void init() override {
             auto tab = gui::MenuTab::find("Level");
 
             tab->addToggle("Hide Level Complete VFX", "level.hidelevelcomplete")
-                ->setDescription("Hides the explosion and fireworks seen when completing a level. (Does not hide particles.) (Created by RayDeeUx)")
+                ->setDescription("Hides the explosion, fireworks, and particles seen when completing a level. (Created by RayDeeUx)")
                 ->handleKeybinds();
         }
 
@@ -21,38 +22,82 @@ namespace eclipse::hacks::Level {
 
     REGISTER_HACK(HideLevelCompleteVFX)
 
-
     class $modify(HideLevelCompleteVFXCCCWHook, CCCircleWave) {
-        static CCCircleWave* create(float startRadius, float endRadius, float duration, bool fadeIn, bool easeOut) {
-            CCCircleWave* cw = CCCircleWave::create(startRadius, endRadius, duration, fadeIn, easeOut);
+        void setPosition(cocos2d::CCPoint const& p0) {
+            CCCircleWave::setPosition(p0);
+
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return;
+
             PlayLayer* pl = PlayLayer::get();
-            if (!pl) return cw;
 
-            if (pl->m_levelEndAnimationStarted && config::get<bool>("level.hidelevelcomplete", false))
-                cw->setVisible(false);
+            if (!pl) return;
 
-            return cw;
+            /*
+            CCCircleWaves sometimes get added
+            as child of CurrencyRewardLayer when completing rated levels
+            so i had to change hooks to CCCircleWave::draw();
+
+            this doesnt affect CCCircleWaves when playlayer is active
+            thanks to the m_levelEndAnimationStarted member variable
+            */
+            if (pl->m_levelEndAnimationStarted && !geode::cast::typeinfo_cast<CurrencyRewardLayer*>(this->getParent()))
+                this->setVisible(false);
         }
     };
 
     class $modify(HideLevelCompleteVFXCCLFHook, CCLightFlash) {
-        // i cant believe i need to hook this function with NINETEEN params to get things working what the heck :despair:
+        // i cant believe i need to hook this function with TWENTY params to get things working what the heck :despair:
         void playEffect(cocos2d::CCPoint point, cocos2d::ccColor3B color, float p2, float p3, float p4, float p5, float p6, float p7, float p8, float p9, float p10, float p11, float p12, float p13, float p14, float p15, int p16, bool p17, bool p18, float p19) {
+            CCLightFlash::playEffect(
+                point, color, p2, p3, p4, p5, p6, p7, p8, p9, p10,
+                p11, p12, p13, p14, p15, p16, p17, p18, p19
+            );
+
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return;
+
             PlayLayer* pl = PlayLayer::get();
 
-            if (!pl)
-                return CCLightFlash::playEffect(
-                    point, color, p2, p3, p4, p5, p6, p7, p8, p9, p10,
-                    p11, p12, p13, p14, p15, p16, p17, p18, p19
-                );
+            if (!pl) return;
 
-            if (pl->m_levelEndAnimationStarted && config::get<bool>("level.hidelevelcomplete", false))
+            if (pl->m_levelEndAnimationStarted)
                 this->setVisible(false);
+        }
+    };
 
-            CCLightFlash::playEffect(
-                point, color, p2, p3, p4, p5, p6, p7, p8, p9,
-                p10, p11, p12, p13, p14, p15, p16, p17, p18, p19
-            );
+    // in case of setupVBO() emergency, comment the below out
+    // nvm it hooks but draw() is finicky and create() doesnt work
+    /*
+    class $modify(HideLevelCompleteVFXCCPSQHook, cocos2d::CCParticleSystemQuad) {
+        void initTexCoordsWithRect(cocos2d::CCRect const& p0) {
+            CCParticleSystemQuad::initTexCoordsWithRect(p0);
+            geode::log::info("help");
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return;
+            PlayLayer* pl = PlayLayer::get();
+            if (!pl) return;
+            if (this->getParent() != pl) return;
+
+            if (pl->m_levelEndAnimationStarted)
+                this->setVisible(false);
+        }
+    };
+    */
+
+    // backup plan in case something goes terribly wrong with setupVBO() again --raydeeux
+    // nvm activate the backup plan NOW bc stuff won't hook
+    class $modify(HideLevelCompleteVFXCCPSHook, cocos2d::CCParticleSystem) {
+        void initParticle(cocos2d::sCCParticle* p0) {
+            CCParticleSystem::initParticle(p0);
+
+            if (!config::get<bool>("level.hidelevelcomplete", false)) return;
+
+            PlayLayer* pl = PlayLayer::get();
+
+            if (!pl) return;
+            if (this->getParent() != pl) return;
+            if (!geode::cast::typeinfo_cast<cocos2d::CCParticleSystemQuad*>(this)) return;
+
+            if (pl->m_levelEndAnimationStarted)
+                this->setVisible(false);
         }
     };
 }
