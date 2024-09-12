af588c020b5c8b41a9c58168d5a2aff9e889816e
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2024-09-12 13:27:44 -0400

    fix noclip, add custom object bypass

--- src/hacks/Player/ShowTrajectory.cpp
+++ src/hacks/Player/ShowTrajectory.cpp
@@ -16,9 +16,15 @@ namespace eclipse::hacks::Player {
         void init() override {
             auto tab = gui::MenuTab::find("Player");
 
-            tab->addToggle("Show Trajectory", "player.showtrajectory")
+            gui::ToggleComponent* toggle = tab->addToggle("Show Trajectory", "player.showtrajectory")
                 ->setDescription("Shows where the player will be if they click/don't click.")
                 ->handleKeybinds();
+
+            config::setIfEmpty("player.showtrajectory.iterations", 300);
+
+            toggle->addOptions([](std::shared_ptr<gui::MenuTab> options) {
+                options->addInputInt("Iterations", "player.showtrajectory.iterations", 1, 1000);
+            });
         }
 
         [[nodiscard]] bool isCheating() override { return config::get<bool>("player.showtrajectory", false); }
@@ -103,7 +109,8 @@ namespace eclipse::hacks::Player {
 
             bool iterationActionDone = false;
 
-            const size_t iterations = 300;
+            const size_t iterations = config::get<int>("player.showtrajectory.iterations", 300);
+;
 
             for (size_t i = 0; i < iterations; i++) {
                 cocos2d::CCPoint initialPlayerPosition = player->getPosition();
