b3d5d545da5faa2c85bb68d6b3b611ae7a24ea3b
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2024-10-11 18:18:36 -0400

    unnecessary nesting

--- src/hacks/Shortcuts/Shortcuts.cpp
+++ src/hacks/Shortcuts/Shortcuts.cpp
@@ -18,76 +18,76 @@ namespace eclipse::hacks::Shortcuts {
         }
 
         static void uncompleteLevel() {
-            if (auto scene = cocos2d::CCScene::get()) { // sometimes people frget CCScene can sometimes be nullptr for no reason
-                GJGameLevel* level = nullptr;
+            auto scene = cocos2d::CCScene::get();
+            if (!scene) return; // sometimes people frget CCScene can sometimes be nullptr for no reason
+            GJGameLevel* level = nullptr;
 
-                // try to find it from either PlayLayer or LevelInfoLayer
-                auto* pl = PlayLayer::get();
-                if (pl) {
-                    level = pl->m_level;
-                } else if (auto* lil = geode::cocos::getChildOfType<LevelInfoLayer>(scene, 0)) {
-                    level = lil->m_level;
-                }
-
-                if (!level) return Popup::create("Error", "You have to open the level first!");
-
-                Popup::create(
-                    "Uncomplete level?",
-                    fmt::format("Are you sure you want to uncomplete \"{}\"?\nYou can't undo this process.", level->m_levelName),
-                    "Yes", "No",[level](bool yes) {
-                        if (!yes) return;
-                        auto gsm = GameStatsManager::sharedState();
-                        auto glm = GameLevelManager::sharedState();
-                        // if level had been completed, ensure that their stars are also removed if the said level was rated
-                        if (level->m_normalPercent >= 100 && gsm->hasCompletedLevel(level)) {
-                            int levelid = level->m_levelID.value();
-                            // Delete completion
-                            gsm->setStat("4", gsm->getStat("4") - 1); // completed levels
-                            gsm->m_completedLevels->removeObjectForKey(fmt::format("c_{}", levelid));
-                            if (level->m_stars > 0) {
-                                //GSM->m_completedLevels->removeObjectForKey(fmt::format("unique_{}", levelid));
-                                gsm->m_completedLevels->removeObjectForKey(fmt::format("star_{}", levelid));
-                                gsm->m_completedLevels->removeObjectForKey(fmt::format("demon_{}", levelid));
-                                if (level->isPlatformer()) {
-                                    gsm->setStat("28", gsm->getStat("28") - level->m_stars); // moons
-                                } else {
-                                    gsm->setStat("6", gsm->getStat("6") - level->m_stars); // stars
-                                }
-                                if (level->m_demon > 0) {
-                                    gsm->setStat("5", gsm->getStat("5") - 1); // demons
-                                }
-                                gsm->setStat("8", gsm->getStat("8") - level->m_coinsVerified);
-                            }
-                        }
-
-                        // Clear progress
-                        level->m_practicePercent = 0;
-                        level->m_normalPercent = 0;
-                        level->m_newNormalPercent2 = 0;
-                        level->m_orbCompletion = 0;
-                        level->m_54 = 0;
-                        level->m_k111 = 0;
-                        level->m_bestPoints = 0;
-                        level->m_bestTime = 0;
-
-                        /* // unsure if this is needed
-                        level->m_isChkValid = 0;
-                        level->m_chk = 0;
-                        level->m_coinsVerified = 0;
-                        */
-
-                        // Remove coins
-                        auto coinDict = gsm->m_verifiedUserCoins;
-                        for (auto i = 0; i < level->m_coins; i++) {
-                            auto key = level->getCoinKey(i + 1);
-                            coinDict->removeObjectForKey(key);
-                        }
-
-                        // Save the level
-                        glm->saveLevel(level);
-                    }
-                );
+            // try to find it from either PlayLayer or LevelInfoLayer
+            auto* pl = PlayLayer::get();
+            if (pl) {
+                level = pl->m_level;
+            } else if (auto* lil = geode::cocos::getChildOfType<LevelInfoLayer>(scene, 0)) {
+                level = lil->m_level;
             }
+
+            if (!level) return Popup::create("Error", "You have to open the level first!");
+
+            Popup::create(
+                "Uncomplete level?",
+                fmt::format("Are you sure you want to uncomplete \"{}\"?\nYou can't undo this process.", level->m_levelName),
+                "Yes", "No",[level](bool yes) {
+                    if (!yes) return;
+                    auto gsm = GameStatsManager::sharedState();
+                    auto glm = GameLevelManager::sharedState();
+                    // if level had been completed, ensure that their stars are also removed if the said level was rated
+                    if (level->m_normalPercent >= 100 && gsm->hasCompletedLevel(level)) {
+                        int levelid = level->m_levelID.value();
+                        // Delete completion
+                        gsm->setStat("4", gsm->getStat("4") - 1); // completed levels
+                        gsm->m_completedLevels->removeObjectForKey(fmt::format("c_{}", levelid));
+                        if (level->m_stars > 0) {
+                            //GSM->m_completedLevels->removeObjectForKey(fmt::format("unique_{}", levelid));
+                            gsm->m_completedLevels->removeObjectForKey(fmt::format("star_{}", levelid));
+                            gsm->m_completedLevels->removeObjectForKey(fmt::format("demon_{}", levelid));
+                            if (level->isPlatformer()) {
+                                gsm->setStat("28", gsm->getStat("28") - level->m_stars); // moons
+                            } else {
+                                gsm->setStat("6", gsm->getStat("6") - level->m_stars); // stars
+                            }
+                            if (level->m_demon > 0) {
+                                gsm->setStat("5", gsm->getStat("5") - 1); // demons
+                            }
+                            gsm->setStat("8", gsm->getStat("8") - level->m_coinsVerified);
+                        }
+                    }
+
+                    // Clear progress
+                    level->m_practicePercent = 0;
+                    level->m_normalPercent = 0;
+                    level->m_newNormalPercent2 = 0;
+                    level->m_orbCompletion = 0;
+                    level->m_54 = 0;
+                    level->m_k111 = 0;
+                    level->m_bestPoints = 0;
+                    level->m_bestTime = 0;
+
+                    /* // unsure if this is needed
+                    level->m_isChkValid = 0;
+                    level->m_chk = 0;
+                    level->m_coinsVerified = 0;
+                    */
+
+                    // Remove coins
+                    auto coinDict = gsm->m_verifiedUserCoins;
+                    for (auto i = 0; i < level->m_coins; i++) {
+                        auto key = level->getCoinKey(i + 1);
+                        coinDict->removeObjectForKey(key);
+                    }
+
+                    // Save the level
+                    glm->saveLevel(level);
+                }
+            );
         }
 
         static void restartLevel() {
