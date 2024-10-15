12e745425da93f9443f432b4c473afb3f5524bd0
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-10-14 20:51:37 -0400

    You forgot to eat /// Now you're out and about // But you wanna be discreet // Can't be eatin' omakase in the middle of the street

--- src/hacks/Player/MuteLevelAudioOnDeath.cpp
+++ src/hacks/Player/MuteLevelAudioOnDeath.cpp
@@ -2,7 +2,6 @@
 #include <modules/hack/hack.hpp>
 #include <modules/config/config.hpp>
 
-#include <Geode/modify/PlayerObject.hpp>
 #include <Geode/modify/EffectGameObject.hpp>
 
 namespace eclipse::hacks::Player {
@@ -20,68 +19,6 @@ namespace eclipse::hacks::Player {
     };
 
     REGISTER_HACK(MuteLevelAudioOnDeath)
-
-    class $modify(MuteLevelAudioOnDeathPOHook, PlayerObject) {
-        ADD_HOOKS_DELEGATE("player.mutelevelaudioondeath")
-
-        /*
-        originally from erysedits by raydeeux.
-        adapted by raydeeux, and improved using 2.206's bindings.
-        -- raydeeux
-        */
-        void playerDestroyed(bool p0) {
-            PlayLayer* pl = PlayLayer::get();
-
-            // do nothing if playlayer is nullptr
-            if (!pl) return PlayerObject::playerDestroyed(p0);
-
-            // avoid accidental sfx muting with deaths from others in globed
-            if (this != pl->m_player1 && this != pl->m_player2)
-                return PlayerObject::playerDestroyed(p0);
-
-            // do nothing if in practice mode BUT practice sync disabled
-            if (pl->m_isPracticeMode && !pl->m_practiceMusicSync)
-                return PlayerObject::playerDestroyed(p0);
-
-            const auto fmod = FMODAudioEngine::sharedEngine();
-
-            /*
-            stopAllMusic(), while not inlined, does not represent
-            accurate behavior, and forces music to restart
-            in platformer levels.
-            gd handles restarting music in classic levels.
-            */
-            /*
-            as of october 14, 2024, and as a direct result of the
-            absolute sin of a death effect in level ID 110961285,
-            i am bringing back the stopAllMusic() call.
-            */
-            if (pl->m_isPlatformer) fmod->pauseAllMusic();
-            else fmod->stopAllMusic();
-
-            if (this == pl->m_player2 && pl->m_level->m_twoPlayerMode)
-                /*
-                avoid stopping sfx twice -- thank you clicksounds.
-                this is here in case adam729 ports death sound randomizer,
-                but more importantly because we want to hear player death sfx.
-                */
-                return PlayerObject::playerDestroyed(p0);
-
-            /*
-            originally the function call was stopAllEffects()
-            which should've sufficed for 90% of SFX,
-            but accuracy > getting the job done in case there's
-            SFX that lasts far, FAR longer.
-            */
-            /*
-            as of august 2nd, 2024, it is probably better
-            to call stopAllEffects to get the death SFX playing.
-            */
-            fmod->stopAllEffects();
-
-            PlayerObject::playerDestroyed(p0);
-        }
-    };
 
     class $modify(MuteLevelAudioOnDeathEGOHook, EffectGameObject) {
         ADD_HOOKS_DELEGATE("player.mutelevelaudioondeath")
