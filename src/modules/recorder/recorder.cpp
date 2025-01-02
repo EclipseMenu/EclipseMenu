4fccbad60cd6aaca2d6c782c258c64e349a20ac7
Oleksandr Nemesh <prevter@gmail.com>
2025-01-02 21:43:28 +0200

    add bindings for kazmath functions on mac

--- src/modules/recorder/recorder.cpp
+++ src/modules/recorder/recorder.cpp
@@ -1,27 +1,75 @@
 #include "recorder.hpp"
 
+#include <memory>
+#include <thread>
+#include <utility>
 #include <Geode/binding/FMODAudioEngine.hpp>
 #include <Geode/loader/Log.hpp>
 #include <Geode/utils/general.hpp>
-#include <thread>
-#include <memory>
-#include <utility>
-#include <modules/utils/SingletonCache.hpp>
 #include <modules/recorder/DSPRecorder.hpp>
+#include <modules/utils/SingletonCache.hpp>
+
+namespace km {
+
+// separated to not forget to update both platforms
+#ifdef GEODE_IS_ARM_MAC
+#define BINDINGS_VERSION 22074
+#else
+#define BINDINGS_VERSION 22074
+#endif
+
+#define DEFINE_ADDRESS(name, address, ...) \
+    using func_t = decltype(&name); \
+    constexpr func_t func = (func_t)address;\
+    static_assert(GEODE_COMP_GD_VERSION == BINDINGS_VERSION, "Address for " ##name " is outdated"); \
+    return func(__VA_ARGS__)
+
+    inline void GLMatrixMode(kmGLEnum mode) {
+        #ifdef GEODE_IS_MACOS
+        DEFINE_ADDRESS(kmGLMatrixMode, GEODE_ARM_MAC(0x1aba44) GEODE_INTEL_MAC(0x1a9f44), mode);
+        #else
+        kmGLMatrixMode(mode);
+        #endif
+    }
+
+    inline void GLLoadIdentity() {
+        #ifdef GEODE_IS_MACOS
+        DEFINE_ADDRESS(kmGLLoadIdentity, GEODE_ARM_MAC(0x1abafc) GEODE_INTEL_MAC(0x1f6010));
+        #else
+        kmGLLoadIdentity();
+        #endif
+    }
+
+    inline kmMat4* Mat4OrthographicProjection(kmMat4* pOut, float left, float right, float bottom, float top, float nearVal, float farVal) {
+        #ifdef GEODE_IS_MACOS
+        DEFINE_ADDRESS(kmMat4OrthographicProjection, GEODE_ARM_MAC(0x34a914) GEODE_INTEL_MAC(0x3c1240), pOut, left, right, bottom, top, nearVal, farVal);
+        #else
+        return kmMat4OrthographicProjection(pOut, left, right, bottom, top, nearVal, farVal);
+        #endif
+    }
+
+    inline void GLMultMatrix(const kmMat4* pIn) {
+        #ifdef GEODE_IS_MACOS
+        DEFINE_ADDRESS(kmGLMultMatrix, GEODE_ARM_MAC(0x1abb60) GEODE_INTEL_MAC(0x1f6070));
+        #else
+        kmGLMultMatrix(pIn);
+        #endif
+    }
+}
 
 namespace eclipse::recorder {
     namespace ffmpeg = ffmpeg::events;
 
     class ProjectionDelegate : public cocos2d::CCDirectorDelegate {
         void updateProjection() override {
-            kmGLMatrixMode(KM_GL_PROJECTION);
-            kmGLLoadIdentity();
+            km::GLMatrixMode(KM_GL_PROJECTION);
+            km::GLLoadIdentity();
             kmMat4 orthoMatrix;
             auto size = utils::get<cocos2d::CCDirector>()->m_obWinSizeInPoints;
-            kmMat4OrthographicProjection(&orthoMatrix, 0, size.width, size.height, 0, -1024, 1024);
-            kmGLMultMatrix(&orthoMatrix);
-            kmGLMatrixMode(KM_GL_MODELVIEW);
-            kmGLLoadIdentity();
+            km::Mat4OrthographicProjection(&orthoMatrix, 0, size.width, size.height, 0, -1024, 1024);
+            km::GLMultMatrix(&orthoMatrix);
+            km::GLMatrixMode(KM_GL_MODELVIEW);
+            km::GLLoadIdentity();
         }
     };
 
