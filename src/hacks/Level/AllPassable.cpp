178528c423fb83f4ef43f52c15f9f5034dc2e876
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2025-01-12 21:52:45 -0500

    Add All Passable (#129)
    * Add All Passable
    
    * Make the changes prevter suggested (hopefully i understood properly)
    
    * why did this one go back to true
    
    * here mr spaghett
    
    * i swear i did this

---
+++ src/hacks/Level/AllPassable.cpp
@@ -0,0 +1,54 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+#include <Geode/modify/GameObject.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+namespace eclipse::hacks::Player {
+    class $modify(AllPassGOHook, GameObject) {
+        struct Fields {
+            bool m_isPassable = false;
+            bool checked = false;
+        };
+    };
+
+    class $modify(AllPassPLHook, PlayLayer) {
+        void setupHasCompleted() {
+            PlayLayer::setupHasCompleted();
+            if (!config::get<bool>("level.allpassable", false)) return;
+            for (auto obj : geode::cocos::CCArrayExt<AllPassGOHook *>(m_objects)) {
+                obj->m_fields->m_isPassable = obj->m_isPassable;
+                obj->m_fields->checked = true;
+                obj->m_isPassable = true;
+            }
+        }
+    };
+
+    class AllPassable : public hack::Hack {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.level");
+
+            tab->addToggle("level.allpassable")
+               ->setDescription()
+               ->handleKeybinds()
+               ->callback([] (bool toggled) {
+                if (auto pl = utils::get<PlayLayer>()) {
+                    for (auto obj : geode::cocos::CCArrayExt<AllPassGOHook *>(pl->m_objects)) {
+                        if (!obj->m_fields->checked) {
+                            obj->m_fields->m_isPassable = obj->m_isPassable;
+                            obj->m_fields->checked = true;
+                        }
+                        obj->m_isPassable = toggled || obj->m_fields->m_isPassable;
+                    }
+                }
+               });
+        }
+
+        [[nodiscard]] bool isCheating() override { return config::get<bool>("level.allpassable", false); }
+        [[nodiscard]] const char* getId() const override { return "All Passable"; }
+    };
+
+    REGISTER_HACK(AllPassable)
+}
