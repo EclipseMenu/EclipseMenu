79ac15c7a2e8acc72d97dac11f28007b7f1a5cdd
cynthebnuy <cynthebnuy@gmail.com>
2025-02-02 11:25:19 +0700

    test
    gg

---
+++ src/hacks/Level/ConfirmRestart.cpp
@@ -0,0 +1,36 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+#include <Geode/modify/PauseLayer.hpp>
+
+namespace eclipse::hacks::Level {
+    class $hack(ConfirmPractice) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.level");
+            tab->addToggle("level.confirmpractice")->handleKeybinds()->setDescription();
+        }
+
+        [[nodiscard]] const char* getId() const override { return "Confirm Practice"; }
+    };
+
+    REGISTER_HACK(ConfirmPractice)
+
+    class $modify(ConfirmPracticePauseLayerHook, PauseLayer) {
+        ADD_HOOKS_DELEGATE("level.pauselayer")
+
+        void onPracticeMode(cocos2d::CCObject* sender) {
+            geode::createQuickPopup(
+                "Title",            // title
+                "Say hi to mom?",   // content
+                "Nah", "Yeah",      // buttons
+                [](auto, bool btn2) {
+                    if (btn2) {
+                        PauseLayer::onPracticeMode(sender);
+                    }
+                }
+            );
+        }
+    };
+}
