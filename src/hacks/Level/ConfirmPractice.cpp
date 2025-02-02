51b9aa5448801ec907b034bf841c19a636375af7
cynthebnuy <cynthebnuy@gmail.com>
2025-02-02 16:27:18 +0700

    i think this would work
    idk

--- src/hacks/Level/ConfirmPractice.cpp
+++ src/hacks/Level/ConfirmPractice.cpp
@@ -9,7 +9,14 @@ namespace eclipse::hacks::Level {
     class $hack(ConfirmPractice) {
         void init() override {
             auto tab = gui::MenuTab::find("tab.level");
-            tab->addToggle("level.confirmpractice")->handleKeybinds()->setDescription();
+
+            config::setIfEmpty("level.confirmpractice", false);
+            config::setIfEmpty("level.confirmpractice.confirmexitpractice", true);
+
+            tab->addToggle("level.confirmpractice")->handleKeybinds()->setDescription()
+               ->addOptions([](std::shared_ptr<gui::MenuTab> options) {
+                   options->addToggle("level.confirmpractice.confirmexitpractice")->setDescription();
+               });
         }
 
         [[nodiscard]] const char* getId() const override { return "Confirm Practice"; }
@@ -20,15 +27,16 @@ namespace eclipse::hacks::Level {
     class $modify(ConfirmPracticePauseLayerHook, PauseLayer) {	
         // ADD_HOOKS_DELEGATE("level.confirmpractice")
 
-	struct Fields {
-            bool m_isPopupVisible = false;
-	};
+	    struct Fields {
+                bool m_isEnterPopupVisible = false;
+                bool m_isExitPopupVisible = false;
+	    };
 
-	void onPracticeMode(cocos2d::CCObject* sender) {
-            if (m_fields->m_isPopupVisible) {
+	    void onPracticeMode(cocos2d::CCObject* sender) {
+            if (m_fields->m_isEnterPopupVisible) {
                 PauseLayer::onPracticeMode(sender);
                 return;
-		}
+		    }
 
             geode::createQuickPopup(
                 "Did I cook?",          // title
@@ -36,9 +44,29 @@ namespace eclipse::hacks::Level {
                 "Nuh uh", "Yuh huh",    // buttons
                 [this, sender](auto, bool btn2) {
                     if (btn2) {
-                        m_fields->m_isPopupVisible = true;
+                        m_fields->m_isEnterPopupVisible = true;
                         PauseLayer::onPracticeMode(sender);
-                        m_fields->m_isPopupVisible = false;
+                        m_fields->m_isEnterPopupVisible = false;
+                    }
+                }
+            );
+        }
+
+        void onNormalMode(cocos2d::CCObject* sender) {
+            if (m_fields->m_isExitPopupVisible || !config::get<bool>("level.confirmpractice.confirmexitpractice", false)) {
+                PauseLayer::onNormalMode(sender);
+                return;
+            }
+
+            geode::createQuickPopup(
+                "Did I cook again?",    // title
+                "ahh commit",			// content
+                "Nuh uh", "Yuh huh",    // buttons
+                [this, sender](auto, bool btn2) {
+                    if (btn2) {
+                        m_fields->m_isExitPopupVisible = true;
+                        PauseLayer::onNormalMode(sender);
+                        m_fields->m_isExitPopupVisible = false;
                     }
                 }
             );
