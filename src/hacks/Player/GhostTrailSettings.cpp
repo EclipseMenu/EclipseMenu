d95d0ba58fd81cc7c3d5b61a63a0cc077cc24d21
Cyn :3 <cynthebnuy@gmail.com>
2025-02-15 09:07:17 +0700

    Hide Player, Force Ghost Trail & fixing No Ghost Trail & No Hide Trigger (#151)
    * Added Force Ghost Trail
    
    * きゅうくらりん
    
    * Update HidePlayer.cpp
    
    * Create ForceGhostTrail.cpp
    
    * Disable ghost trail after death
    
    * fxck around and find out
    
    * Update ForceGhostTrail.cpp (Thanks Jas ^^)
    
    * damn it
    
    * bruh
    
    * this should work
    
    * i think this should work now :3
    
    * begging
    
    * should be done i think
    
    * i think it works now
    
    * Delete src/hacks/Player/NoGhostTrail.cpp
    
    * Update and rename HidePlayer.cpp to HideOrShowPlayer.cpp
    
    * Delete src/hacks/Player/NoHideTrigger.cpp
    
    * damn it
    
    * cursed member access fixed lol
    
    * Update GhostTrailSettings.cpp
    
    * added cheat indicator

---
+++ src/hacks/Player/GhostTrailSettings.cpp
@@ -0,0 +1,78 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+#include <Geode/modify/PlayerObject.hpp>
+
+namespace eclipse::hacks::Player {
+    class $modify(PlayerObjectGTHook, PlayerObject){
+        struct Fields {
+            GhostType m_curGhostType = GhostType::Disabled;
+        };
+
+        void toggleGhostEffect(GhostType p0) {
+            m_fields->m_curGhostType = p0;
+            PlayerObject::toggleGhostEffect(p0);
+        }
+    };
+
+    class $hack(ForceGhostTrail) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.player");
+            tab->addToggle("player.forceghosttrail")->setDescription()->handleKeybinds()
+            ->callback([](bool v) {
+                config::set("player.noghosttrail", false);
+                auto* gjbgl = utils::get<GJBaseGameLayer>();
+                if (!gjbgl) return;
+
+                auto p1 = static_cast<PlayerObjectGTHook*>(gjbgl->m_player1);
+                auto p2 = static_cast<PlayerObjectGTHook*>(gjbgl->m_player2);
+                p1->toggleGhostEffect(p1->m_fields->m_curGhostType);
+                if (gjbgl->m_gameState.m_isDualMode) p2->toggleGhostEffect(p2->m_fields->m_curGhostType);
+            });
+        }
+      
+        [[nodiscard]] const char* getId() const override { return "Force Ghost Trail"; }
+    };
+
+    class $hack(NoGhostTrail) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.player");
+            tab->addToggle("player.noghosttrail")->setDescription()->handleKeybinds()
+            ->callback([](bool v) {
+                config::set("player.forceghosttrail", false);
+                auto* gjbgl = utils::get<GJBaseGameLayer>();
+                if (!gjbgl) return;
+
+                auto p1 = static_cast<PlayerObjectGTHook*>(gjbgl->m_player1);
+                auto p2 = static_cast<PlayerObjectGTHook*>(gjbgl->m_player2);
+                p1->toggleGhostEffect(p1->m_fields->m_curGhostType);
+                if (gjbgl->m_gameState.m_isDualMode) p2->toggleGhostEffect(p2->m_fields->m_curGhostType);
+            });
+        }
+      
+        [[nodiscard]] const char* getId() const override { return "No Ghost Trail"; }
+    };
+
+    REGISTER_HACK(ForceGhostTrail)
+    REGISTER_HACK(NoGhostTrail)
+
+    class $modify(PlayerObjectFGTHookImpl, PlayerObject){
+        ADD_HOOKS_DELEGATE("player.forceghosttrail");
+
+        void toggleGhostEffect(GhostType p0) {
+            if (!this->m_isDead) p0 = GhostType::Enabled;
+            PlayerObject::toggleGhostEffect(p0);
+        }
+    };
+
+    class $modify(PlayerObjectNGTHookImpl, PlayerObject){
+        ADD_HOOKS_DELEGATE("player.noghosttrail");
+
+        void toggleGhostEffect(GhostType p0) {
+            p0 = GhostType::Disabled;
+            PlayerObject::toggleGhostEffect(p0);
+        }
+    };
+}
---
+++ src/hacks/Player/HideOrShowPlayer.cpp
@@ -0,0 +1,78 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+#include <Geode/modify/PlayerObject.hpp>
+
+namespace eclipse::hacks::Player {
+    class $modify(PlayerObjectTVHook, PlayerObject){
+        struct Fields {
+            bool m_isPlayerInvis = false;
+        };
+
+        void toggleVisibility(bool p0) {
+            m_fields->m_isPlayerInvis = p0;
+            PlayerObject::toggleVisibility(p0);
+        }
+    };
+
+    class $hack(HidePlayer) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.player");
+            tab->addToggle("player.hideplayer")->setDescription()->handleKeybinds()
+            ->callback([](bool v) {
+                config::set("player.nohidetrigger", false);
+                auto* gjbgl = utils::get<GJBaseGameLayer>();
+                if (!gjbgl) return;
+
+                auto p1 = static_cast<PlayerObjectTVHook*>(gjbgl->m_player1);
+                auto p2 = static_cast<PlayerObjectTVHook*>(gjbgl->m_player2);
+                p1->toggleVisibility(p1->m_fields->m_isPlayerInvis);
+                if (gjbgl->m_gameState.m_isDualMode) p2->toggleVisibility(p2->m_fields->m_isPlayerInvis);
+            });
+        }
+      
+        [[nodiscard]] const char* getId() const override { return "Hide Player"; }
+    };
+
+    class $hack(NoHideTrigger) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.player");
+            tab->addToggle("player.nohidetrigger")->setDescription()->handleKeybinds()
+            ->callback([](bool v) {
+                config::set("player.hideplayer", false);
+                auto* gjbgl = utils::get<GJBaseGameLayer>();
+                if (!gjbgl) return;
+
+                auto p1 = static_cast<PlayerObjectTVHook*>(gjbgl->m_player1);
+                auto p2 = static_cast<PlayerObjectTVHook*>(gjbgl->m_player2);
+                p1->toggleVisibility(p1->m_fields->m_isPlayerInvis);
+                if (gjbgl->m_gameState.m_isDualMode) p2->toggleVisibility(p2->m_fields->m_isPlayerInvis);
+            });
+        }
+        [[nodiscard]] bool isCheating() const override {
+            return config::get<"player.nohidetrigger", bool>();
+        }
+        [[nodiscard]] const char* getId() const override { return "No Hide Trigger"; }
+    };
+
+    REGISTER_HACK(HidePlayer)
+    REGISTER_HACK(NoHideTrigger)
+
+    class $modify(PlayerObjectHPHookImpl, PlayerObject){
+        ADD_HOOKS_DELEGATE("player.hideplayer");
+
+        void toggleVisibility(bool p0) {
+            PlayerObject::toggleVisibility(false);
+        }
+    };
+
+    class $modify(PlayerObjectNHTHookImpl, PlayerObject){
+        ADD_HOOKS_DELEGATE("player.nohidetrigger");
+
+        void toggleVisibility(bool p0) {
+            PlayerObject::toggleVisibility(true);
+        }
+    };
+}
