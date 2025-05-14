fb13f588f306f92e9cfffa4877a8e2a33a91801e
prevter <prevter@gmail.com>
2025-05-14 19:05:44 +0300

    improve solid wave trail compat

--- src/hacks/Player/SolidWaveTrail.cpp
+++ src/hacks/Player/SolidWaveTrail.cpp
@@ -5,6 +5,7 @@
 
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCDrawNode.hpp>
+#include <Geode/modify/HardStreak.hpp>
 
 namespace eclipse::hacks::Player {
     class $hack(SolidWaveTrail) {
@@ -18,15 +19,30 @@ namespace eclipse::hacks::Player {
 
     REGISTER_HACK(SolidWaveTrail)
 
+    static bool s_insideUpdateStroke = false;
+
     class $modify(SolidWaveTrailCCDNHook, cocos2d::CCDrawNode) {
         ALL_DELEGATES_AND_SAFE_PRIO("player.solidwavetrail")
 
         bool drawPolygon(cocos2d::CCPoint* p0, unsigned int p1, const cocos2d::ccColor4F& p2, float p3, const cocos2d::ccColor4F& p4) {
+            if (!s_insideUpdateStroke) return CCDrawNode::drawPolygon(p0, p1, p2, p3, p4);
+
             if (p2.r == 1.F && p2.g == 1.F && p2.b == 1.F && p2.a != 1.F) return true;
 
-            this->setBlendFunc(cocos2d::CCSprite::create()->getBlendFunc());
+            this->setBlendFunc({ CC_BLEND_SRC, CC_BLEND_DST });
             this->setZOrder(-1);
+
             return CCDrawNode::drawPolygon(p0, p1, p2, p3, p4);
+        }
+    };
+
+    class $modify(SolidWaveTrailHSHook, HardStreak) {
+        ALL_DELEGATES_AND_SAFE_PRIO("player.solidwavetrail")
+
+        void updateStroke(float p0) {
+            s_insideUpdateStroke = true;
+            HardStreak::updateStroke(p0);
+            s_insideUpdateStroke = false;
         }
     };
 }
