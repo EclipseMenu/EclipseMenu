8f7858f8a0b16956a163462827b7107d3d417dbb
prevter <prevter@gmail.com>
2025-10-23 23:58:49 +0300

    bump discord-presence version

--- cmake/IncludeLibs.cmake
+++ cmake/IncludeLibs.cmake
@@ -16,7 +16,7 @@ target_sources(imgui INTERFACE
 set(HAS_IMGUI ON)
 
 if (NOT ANDROID AND NOT IOS)
-    CPMAddPackage("gh:EclipseMenu/discord-presence#a4646fb")
+    CPMAddPackage("gh:EclipseMenu/discord-presence#59a3ba7")
 endif()
 CPMAddPackage("gh:matcool/gd-imgui-cocos#47ff1d6")
 CPMAddPackage("gh:maxnut/GDR-converter#55c5321")
