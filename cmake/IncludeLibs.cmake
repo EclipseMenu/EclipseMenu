48bdfe937f3ef12c73933761e86e87c3c9bf1953
prevter <prevter@gmail.com>
2025-11-29 20:31:05 +0200

    update imgui

--- cmake/IncludeLibs.cmake
+++ cmake/IncludeLibs.cmake
@@ -3,7 +3,7 @@ add_library(third_party INTERFACE)
 
 # Include libraries
 add_library(imgui INTERFACE)
-CPMAddPackage("gh:EclipseMenu/imgui#60c5b21")
+CPMAddPackage("gh:EclipseMenu/imgui#6cd1915")
 target_include_directories(imgui INTERFACE ${imgui_SOURCE_DIR})
 target_sources(imgui INTERFACE
     ${imgui_SOURCE_DIR}/imgui.cpp
