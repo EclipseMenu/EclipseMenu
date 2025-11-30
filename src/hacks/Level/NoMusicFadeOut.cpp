4c38a2a7e0eee7834e5c4bf5e2b3a45f2fa265aa
Beat <75857504+BeatACVR@users.noreply.github.com>
2025-11-29 20:46:05 -0600

    Add files via upload

---
+++ src/hacks/Level/NoMusicFadeOut.cpp
@@ -0,0 +1,27 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+#include <Geode/modify/FMODAudioEngine.hpp>
+
+namespace eclipse::hacks::Level {
+    class $hack(NoMusicFadeOut) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.level");
+            tab->addToggle("level.nomusicfadeout")->handleKeybinds()->setDescription();
+        }
+
+        [[nodiscard]] const char* getId() const override { return "No Music Fade Out"; }
+    };
+
+    REGISTER_HACK(NoMusicFadeOut)
+
+    class $modify(NMFOFMODAudioEngineHook, FMODAudioEngine) {
+        ADD_HOOKS_DELEGATE_AND_SAFE_PRIO("level.nomusicfadeout")
+
+        void fadeOutMusic(float duration, int channel) {
+            return;
+        }
+    };
+}
