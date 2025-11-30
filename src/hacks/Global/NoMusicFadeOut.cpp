5326babb0c9a50c07d9dfc634b2b4371bd8151c1
Beat <75857504+BeatACVR@users.noreply.github.com>
2025-11-29 19:56:42 -0600

    Add files via upload

---
+++ src/hacks/Global/NoMusicFadeOut.cpp
@@ -0,0 +1,27 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+#include <Geode/modify/FMODAudioEngine.hpp>
+
+namespace eclipse::hacks::Global {
+    class $hack(Shipcopter) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.global");
+            tab->addToggle("global.nomusicfadeout")->handleKeybinds()->setDescription();
+        }
+
+        [[nodiscard]] const char* getId() const override { return "No Music Fade Out"; }
+    };
+
+    REGISTER_HACK(NoMusicFadeOut)
+
+    class $modify(NMFOFMODAudioEngineHook, FMODAudioEngine) {
+        ADD_HOOKS_DELEGATE("global.nomusicfadeout")
+
+        void fadeOutMusic(float duration, int channel) {
+            return;
+        }
+    };
+}
