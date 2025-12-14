597aaff4b2d6a4d01f0cfb39492662f939ceb8e9
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2025-12-14 15:56:49 -0500

    add persist trail

--- src/hacks/Player/WaveTrailOnDeath.cpp
+++ src/hacks/Player/WaveTrailOnDeath.cpp
@@ -6,13 +6,14 @@
 #include <Geode/modify/PlayerObject.hpp>
 
 namespace eclipse::hacks::Player {
-
+    static bool s_isDead = false;
     class $hack(WaveTrailOnDeath) {
         void init() override {
             auto tab = gui::MenuTab::find("tab.player");
-            tab->addToggle("player.wavetrailondeath")->setDescription()->handleKeybinds();
+            tab->addToggle("player.wavetrailondeath")->setDescription()->handleKeybinds()->addOptions([](auto options) {
+                options->addToggle("player.wavetrailondeath.persist-trail")->setDescription()->handleKeybinds();
+            });
         }
-
         [[nodiscard]] const char* getId() const override { return "Wave Trail on Death"; }
     };
 
@@ -20,8 +21,19 @@ namespace eclipse::hacks::Player {
 
     class $modify(WaveTrailOnDeathPOHook, PlayerObject) {
         ALL_DELEGATES_AND_SAFE_PRIO("player.wavetrailondeath")
-        void fadeOutStreak2(float duration) {}
-        void createFadeOutDartStreak() {}
+        void playerDestroyed(bool p0) {
+            if (auto* pl = utils::get<PlayLayer>())
+                s_isDead = this == pl->m_player1 || this == pl->m_player2;
+            PlayerObject::playerDestroyed(p0);
+            s_isDead = false;
+        }
+        void fadeOutStreak2(float duration) {
+            if (s_isDead || config::get<bool>("player.wavetrailondeath.persist-trail", false)) return;
+            PlayerObject::fadeOutStreak2(duration);
+        }
+        void createFadeOutDartStreak() {
+            if (s_isDead || config::get<bool>("player.wavetrailondeath.persist-trail", false)) return;
+            PlayerObject::createFadeOutDartStreak();
+        }
     };
-
 }
