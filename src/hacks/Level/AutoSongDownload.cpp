6e42329d635efbd3ded400d44e2f1490c741ff9e
Oleksandr Nemesh <prevter@gmail.com>
2026-02-24 22:12:20 +0200

    fix

--- src/hacks/Level/AutoSongDownload.cpp
+++ src/hacks/Level/AutoSongDownload.cpp
@@ -21,7 +21,8 @@ namespace eclipse::hacks::Level {
         ADD_HOOKS_DELEGATE("level.autosongdownload")
 
         void tryDownload() {
-            if (!GameManager::get()->getGameVariable(GameVar::ShownNewgroundsDisclaimer)) return;
+            if (!utils::get<GameManager>()->getGameVariable(GameVar::ShownNewgroundsDisclaimer))
+                return;
             if (m_songWidget->m_downloadBtn->isVisible())
                 m_songWidget->m_downloadBtn->activate();
         }
