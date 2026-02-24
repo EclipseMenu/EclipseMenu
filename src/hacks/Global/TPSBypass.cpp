8a826acf5b6de16c306659986b5f4fd21a2ae5fe
prevter <prevter@gmail.com>
2026-02-24 04:03:01 +0200

    port physics bypass on some platforms

--- src/hacks/Global/TPSBypass.cpp
+++ src/hacks/Global/TPSBypass.cpp
@@ -1,4 +1,3 @@
-#if 0
 #include <modules/config/config.hpp>
 #include <modules/gui/gui.hpp>
 #include <modules/gui/components/float-toggle.hpp>
@@ -10,8 +9,8 @@
 
 #include <sinaps.hpp>
 
-constexpr float MIN_TPS = 0.f;
-constexpr float MAX_TPS = 100000.f;
+constexpr double MIN_TPS = 0.f;
+constexpr double MAX_TPS = 1000000.f;
 
 #ifdef GEODE_IS_MACOS
 #define REQUIRE_MODIFIED_DELTA_PATCH
@@ -72,13 +71,14 @@ namespace eclipse::hacks::Global {
             auto baseSize = utils::getBaseSize();
 
             // this patch allows us to manually set the expected amount of ticks per update call
-            intptr_t addr = -1;
+            intptr_t addr = sinaps::not_found;
             std::vector<uint8_t> bytes;
             #ifdef GEODE_IS_WINDOWS
             {
                 using namespace assembler::x86_64;
                 // 2.2074: 0x232294
-                addr = sinaps::find<"FF 90 ? ? ? ? ^ F3 0F 10 8E ? ? ? ? F3 44 0F 10">(base, baseSize);
+                // 2.2081: 0x237a55
+                addr = sinaps::find<"FF 90 ? ? ? ? ^ F3 0F 10 ? ? ? ? ? F3 44 0F 10 ? ? ? ? 00 F3 41 0F 5D">(base, baseSize);
                 if (addr != sinaps::not_found) {
                     bytes = Builder(addr)
                         .movabs(Register64::rax, std::bit_cast<uint64_t>(&g_expectedTicks))
@@ -91,24 +91,26 @@ namespace eclipse::hacks::Global {
             #elif defined(GEODE_IS_ANDROID64)
             {
                 using namespace assembler::arm64;
-                // 2.2074: 0x87DA40 (google) / 0x87BE28 (amazon)
+                // 2.2074: 0x87dA40 (google) / 0x87be28 (amazon)
+                // 2.2081: 0x89d9f4
                 auto func = dlsym(RTLD_DEFAULT, "_ZN15GJBaseGameLayer6updateEf");
-                addr = sinaps::find<"0B 19 2B 1E 0F 10 62 1E 00 10 2E 1E">(static_cast<const uint8_t*>(func), 0x500);
+                addr = sinaps::find<"AB 19 60 1E 0A 10 62 1E 6A 09 6A 1E">(static_cast<const uint8_t*>(func), 0x500);
                 if (addr != sinaps::not_found) {
                     addr += reinterpret_cast<intptr_t>(func) - reinterpret_cast<intptr_t>(base); // we need offset from the base
                     bytes = Builder(addr)
                         .mov(Register::x9, std::bit_cast<uint64_t>(&g_expectedTicks))
                         .ldr(Register::w0, Register::x9)
-                        .b(addr + 0x2c, true)
+                        .b(addr + 0x28, true)
                         .build();
                 }
             }
             #elif defined(GEODE_IS_ANDROID32)
             {
                 using namespace assembler::armv7;
-                // 2.2074: 0x4841BC (google) / 0x483F0C (amazon)
+                // 2.2074: 0x4841bc (google) / 0x483f0c (amazon)
+                // 2.2081: 0x49725c
                 auto func = dlsym(RTLD_DEFAULT, "_ZN15GJBaseGameLayer6updateEf");
-                addr = sinaps::find<"B7 EE C7 7A 27 EE 06 7B ^ F7 EE C7 7B 17 EE 90 0A">(static_cast<const uint8_t*>(func), 0x500);
+                addr = sinaps::find<"EE ? ? ? EE ? ? ^ F7 EE ? 7B 17 EE 90 0A">(static_cast<const uint8_t*>(func), 0x500);
                 if (addr != sinaps::not_found) {
                     addr += reinterpret_cast<intptr_t>(func) - reinterpret_cast<intptr_t>(base); // we need offset from the base
                     bytes = Builder(addr)
@@ -122,6 +124,7 @@ namespace eclipse::hacks::Global {
             {
                 using namespace assembler::arm64;
                 // 2.2074: 0x200C30 (iOS) / 0x119454 (macOS)
+                static_assert(false, "TODO: fix for 2.2081");
                 addr = sinaps::find<"00 19 20 1E 02 10 22 1E">(base, baseSize);
                 if (addr != sinaps::not_found) {
                     bytes = Builder(addr)
@@ -134,14 +137,14 @@ namespace eclipse::hacks::Global {
             #elif defined(GEODE_IS_INTEL_MAC)
             {
                 using namespace assembler::x86_64;
-                // 2.2074: 0x14233E
-                addr = sinaps::find<"0F 28 C5 F3 0F 5D 83 ? ? ? ? F3 0F 5E D8">(base, baseSize);
+                // 2.2074: 0x14233e
+                // 2.2081: 0x1516c4
+                addr = sinaps::find<"0F 28 ? F3 0F 5D 83 ? ? ? ? F3 0F ? ? F2 0F 10">(base, baseSize);
                 if (addr != sinaps::not_found) {
                     bytes = Builder(addr)
                         .movabs(Register64::rax, std::bit_cast<uint64_t>(&g_expectedTicks))
                         .movss(XmmRegister::xmm0, Register64::rax)
-                        .jmp(addr + 0x36, true)
-                        .nop(3)
+                        .jmp(addr + 0x49, true)
                         .build();
                 }
             }
@@ -199,104 +202,65 @@ namespace eclipse::hacks::Global {
         #ifdef REQUIRE_MODIFIED_DELTA_PATCH
 
         // function to quickly get the bytes for the patch
-        #ifdef GEODE_IS_INTEL_MAC
-        template <typename T>
         [[nodiscard]] static std::vector<uint8_t> TPStoBytes() {
-            if constexpr (std::is_same_v<T, float>) {
-                return geode::toBytes(1.f / float(config::get<"global.tpsbypass", double>(240.f)));
-            } else if constexpr (std::is_same_v<T, double>) {
-                return geode::toBytes<double>(1.0 / config::get<"global.tpsbypass", double>(240.f));
-            } else {
-                static_assert(alwaysFalse<T>, "TPStoBytes only supports float and double");
-            }
+            return geode::toBytes<double>(1.0 / config::get<"global.tpsbypass", double>(240.0));
         }
-        #elif defined(GEODE_IS_ARM_MAC)
-        template <typename T>
-        [[nodiscard]] static std::vector<uint8_t> TPStoBytes() {
-            if constexpr (std::is_same_v<T, float>) {
-                auto bytes = assembler::arm64::mov_float(
-                    assembler::arm64::Register::w9,
-                    1.f / config::get<"global.tpsbypass", double>(240.f)
-                );
-                return std::vector(bytes.begin(), bytes.end());
-            } else if constexpr (std::is_same_v<T, double>) {
-                auto bytes = assembler::arm64::mov_double(
-                    assembler::arm64::Register::x9,
-                    1.0 / config::get<"global.tpsbypass", double>(240.f)
-                );
-                return std::vector(bytes.begin(), bytes.end());
-            } else {
-                static_assert(alwaysFalse<T>, "TPStoBytes only supports float and double");
-            }
-        }
-        #endif
 
         geode::Result<> setupModifiedDeltaPatches() {
             auto base = reinterpret_cast<uint8_t*>(geode::base::get());
             auto moduleSize = utils::getBaseSize();
-            geode::log::debug("TPSBypass: base = 0x{:X}", (uintptr_t)base);
-            geode::log::debug("TPSBypass: moduleSize = 0x{:X}", moduleSize);
 
             using namespace sinaps::mask;
 
             #ifdef GEODE_IS_INTEL_MAC
             // on x86, we need to replace two values that are used in GJBaseGameLayer::getModifiedDelta
             // pretty simple, just find original values and replace them with raw bytes
+            // UPD 2.2081: float value is no longer there, so only patch double value
 
-            auto floatAddr = sinaps::find<dword<0x3B888889>>(base, moduleSize);
-            auto doubleAddr = sinaps::find<qword<0x3F71111120000000>>(base, moduleSize);
-            #elif defined(GEODE_IS_ARM_MAC)
-            // on ARM, it's a bit tricky, since the value is not stored as a constant.
-            //
-            // assembly in question:
-            // ; Load 0.0041667f
-            // 29 11 91 52        movz w9, #0x8889
-            // 09 71 A7 72        movk w9, #0x3b88, lsl #16
-            // ...
-            // ; Load 0.00416666688
-            // 09 00 A4 D2        movz x9, #0x2000, lsl #16
-            // 29 22 C2 F2        movk x9, #0x1111, lsl #32
-            // 29 EE E7 F2        movk x9, #0x3f71, lsl #48
-            // note that double value only stores upper 48 bits, so we're losing some precision here :(
-
-            auto floatAddr = sinaps::find<"29 11 91 52 09 71 A7 72">(base, moduleSize);
-            auto doubleAddr = sinaps::find<"09 00 A4 D2 29 22 C2 F2 29 EE E7 F2">(base, moduleSize);
-            #endif
-
-            if (floatAddr == sinaps::not_found || doubleAddr == sinaps::not_found) {
-                return geode::Err(fmt::format("failed to find addresses: float = 0x{:X}, double = 0x{:X}", floatAddr, doubleAddr));
+            auto addr = sinaps::find<qword<0x3F71111111111111>>(base, moduleSize);
+            if (addr == sinaps::not_found) {
+                return geode::Err("failed to find getModifiedDelta patch address");
             }
 
-            geode::log::debug("TPSBypass: floatAddr = 0x{:X}", floatAddr);
-            geode::log::debug("TPSBypass: doubleAddr = 0x{:X}", doubleAddr);
-
-            auto patch1Res = geode::Mod::get()->patch(floatAddr + base, TPStoBytes<float>());
-            if (!patch1Res) return geode::Err(fmt::format("failed to patch float address: {}", patch1Res.unwrapErr()));
-            auto patch1 = patch1Res.unwrap();
-            (void) patch1->disable();
-
-            auto patch2Res = geode::Mod::get()->patch(doubleAddr + base, TPStoBytes<double>());
-            if (!patch2Res) return geode::Err(fmt::format("failed to patch double address: {}", patch2Res.unwrapErr()));
-            auto patch2 = patch2Res.unwrap();
-            (void) patch2->disable();
-
-            const auto setState = [patch1, patch2](bool state) {
-                if (state) {
-                    (void) patch1->enable();
-                    (void) patch2->enable();
-                } else {
-                    (void) patch1->disable();
-                    (void) patch2->disable();
-                }
-            };
-
-            const auto setValue = [patch1, patch2]() {
-                (void) patch1->updateBytes(TPStoBytes<float>());
-                (void) patch2->updateBytes(TPStoBytes<double>());
-            };
+            auto patchRes = geode::Mod::get()->patch(addr + base, TPStoBytes());
+            if (!patchRes) return geode::Err(fmt::format("failed to patch address: {}", patchRes.unwrapErr()));
+            auto patch = patchRes.unwrap();
+            (void) patch->disable();
 
             // update bytes on value change
-            config::addDelegate("global.tpsbypass", [setValue] { setValue(); });
+            config::addDelegate("global.tpsbypass", [patch]() { (void) patch->updateBytes(TPStoBytes()); });
+            auto setState = [patch](bool state) { (void) patch->toggle(state); };
+
+            #elif defined(GEODE_IS_ARM_MAC)
+            // on ARM, the value is stored as immediate, with some encoding:
+            // 0x0000000100122C54:  E9 E3 00 B2    orr  x9, xzr, #0x1111111111111111
+            // 0x0000000100122C58:  29 EE E7 F2    movk x9, #0x3f71, lsl #48
+            //
+            // this makes it kinda hard to inject any value we want, so instead we're going to store it
+            // in a GJBaseGameLayer member, load it with one instruction and then restore it in the closest hook
+            //
+            // i'm using `GJGameLoadingLayer* m_loadingLayer`, because it's not used anywhere apart from open/exit sequence,
+            // and should be fine for our use case. since it's a pointer, we can store double right in it
+            // (good luck whoever's going to dereference it lol)
+
+            auto addr = sinaps::find<"E9 E3 00 B2 29 EE E7 F2">(base, moduleSize);
+            if (addr == sinaps::not_found) {
+                return geode::Err("failed to find getModifiedDelta patch address");
+            }
+
+            using namespace assembler::arm64;
+            auto bytes = Builder(addr)
+                .ldr(Register::x9, Register::x19, offsetof(GJBaseGameLayer, m_loadingLayer))
+                .nop()
+                .build();
+
+            auto patchRes = geode::Mod::get()->patch(addr + base, std::move(bytes));
+            if (!patchRes) return geode::Err(fmt::format("failed to patch address: {}", patchRes.unwrapErr()));
+            auto patch = patchRes.unwrap();
+            (void) patch->disable();
+
+            auto setState = [patch](bool state) { (void) patch->toggle(state); };
+            #endif
 
             // update patches on enable/disable
             config::addDelegate("global.tpsbypass.toggle", [setState] {
@@ -375,12 +339,22 @@ namespace eclipse::hacks::Global {
             #endif
 
             auto spt = 1.0 / newTPS;
-            auto steps = std::round(fields->m_extraDelta / spt);
+            auto steps = std::floor(fields->m_extraDelta / spt);
             auto totalDelta = steps * spt;
             fields->m_extraDelta -= totalDelta;
             expectedTicks() = steps;
 
+            #ifdef GEODE_IS_ARM_MAC
+            // we're a bit silly here:
+            auto originalLoadingLayer = m_loadingLayer;
+            this->m_loadingLayer = std::bit_cast<GJGameLoadingLayer*>(1.0 / utils::getTPS());
+            #endif
+
             GJBaseGameLayer::update(totalDelta);
+
+            #ifdef GEODE_IS_ARM_MAC
+            this->m_loadingLayer = originalLoadingLayer;
+            #endif
         }
     };
 
@@ -429,4 +403,3 @@ namespace eclipse::hacks::Global {
         }
     };
 }
-#endif
