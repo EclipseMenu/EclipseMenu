f60bed76a8a31c472a3d8508c30a85efe34df278
prevter <prevter@gmail.com>
2026-02-25 20:21:50 +0200

    fix click teleport

--- src/hacks/Player/ClickTeleport.cpp
+++ src/hacks/Player/ClickTeleport.cpp
@@ -3,23 +3,24 @@
 #include <modules/config/config.hpp>
 #include <modules/gui/gui.hpp>
 #include <modules/gui/components/toggle.hpp>
+#include <modules/gui/components/keybind.hpp>
 #include <modules/hack/hack.hpp>
 #include <modules/keybinds/manager.hpp>
 
 #ifndef GEODE_IS_MOBILE
 
 namespace eclipse::hacks::Player {
-    double degToRad(double degrees) {
+    constexpr double degToRad(double degrees) {
         return degrees * std::numbers::pi / 180;
     }
 
-    cocos2d::CCPoint rotateVector(const cocos2d::CCPoint& vector, double angle) {
+    static cocos2d::CCPoint rotateVector(cocos2d::CCPoint const& vector, double angle) {
         auto x = vector.x * cos(angle) - vector.y * sin(angle);
         auto y = vector.x * sin(angle) + vector.y * cos(angle);
         return ccp(x, y);
     }
 
-    cocos2d::CCPoint screenToGame(const cocos2d::CCPoint& screenPos, PlayLayer* playLayer) {
+    static cocos2d::CCPoint screenToGame(cocos2d::CCPoint const & screenPos, PlayLayer* playLayer) {
         auto cameraPos = playLayer->m_gameState.m_cameraPosition;
         auto cameraScale = playLayer->m_gameState.m_cameraZoom;
         auto cameraAngle = playLayer->m_gameState.m_cameraAngle;
@@ -37,10 +38,10 @@ namespace eclipse::hacks::Player {
         return point;
     }
 
-    cocos2d::CCPoint screenToFrame(const ImVec2& pos) {
+    static cocos2d::CCPoint screenToFrame(ImVec2 const& pos) {
         auto* director = utils::get<cocos2d::CCDirector>();
-        const auto frameSize = director->getOpenGLView()->getFrameSize();
-        const auto winSize = director->getWinSize();
+        auto frameSize = director->getOpenGLView()->getFrameSize();
+        auto winSize = director->getWinSize();
 
         return {
             pos.x / frameSize.width * winSize.width,
@@ -50,23 +51,32 @@ namespace eclipse::hacks::Player {
 
     class $hack(ClickTeleport) {
         void init() override {
+            config::setIfEmpty<keybinds::KeybindProps>("player.clicktp.key", keybinds::Keys::MouseRight);
+
             auto tab = gui::MenuTab::find("tab.player");
-            tab->addToggle("player.clicktp")->setDescription()->handleKeybinds();
+            tab->addToggle("player.clicktp")->setDescription()->handleKeybinds()
+                ->addOptions([](auto options) {
+                    options->addKeybind("player.clicktp.key", "player.clicktp.key")
+                           ->setInternal()
+                           ->setDefaultKey(keybinds::Keys::MouseRight);
+                });
+
+            keybinds::Manager::get()->addListener("player.clicktp.key", [](auto evt) {
+                if (!evt.down) return;
+                if (!config::get<"player.clicktp", bool>(false)) return;
+
+                auto* playLayer = utils::get<PlayLayer>();
+                if (!playLayer) return;
+
+                playLayer->m_player1->m_position = screenToGame(geode::cocos::getMousePos(), playLayer);
+            });
         }
 
         void update() override {
-            if (!config::get<bool>("player.clicktp", false)) return;
+            if (!config::get<"player.clicktp", bool>(false)) return;
 
             // Force the cursor to be visible
             PlatformToolbox::showCursor();
-
-            if (keybinds::isKeyPressed(keybinds::Keys::MouseRight)) {
-                auto playLayer = utils::get<PlayLayer>();
-                if (!playLayer) return;
-                auto gamePos = screenToGame(geode::cocos::getMousePos(), playLayer);
-
-                playLayer->m_player1->m_position = gamePos;
-            }
         }
 
         [[nodiscard]] bool isCheating() const override { return config::get<"player.clicktp", bool>(); }
