d664391e7f08b905ffa6066b6d2443050ea244f2
prevter <prevter@gmail.com>
2026-03-10 23:16:34 +0200

    try to fix label file picker crash

--- src/hacks/Labels/Labels.cpp
+++ src/hacks/Labels/Labels.cpp
@@ -402,39 +402,40 @@ namespace eclipse::hacks::Labels {
                       ->setDescription();
                });
             tab->addButton("labels.import")->callback([this] {
-                geode::async::runtime().spawn([this]() -> arc::Future<> {
-                    geode::utils::file::FilePickOptions::Filter filter;
-                    filter.description = "Eclipse Label (*.ecl)";
-                    filter.files.insert("*.ecl");
+                geode::utils::file::FilePickOptions::Filter filter;
+                filter.description = "Eclipse Label (*.ecl)";
+                filter.files.insert("*.ecl");
 
-                    auto value = co_await geode::utils::file::pick(
+                geode::async::spawn(
+                    geode::utils::file::pick(
                         geode::utils::file::PickMode::OpenFile,
                         {geode::Mod::get()->getSaveDir(), {std::move(filter)}}
-                    );
+                    ),
+                    [this](geode::utils::file::PickResult res) {
+                        if (!res) return;
+                        auto pathOpt = std::move(res).unwrapOr(std::nullopt);
+                        if (!pathOpt.has_value()) return;
 
-                    if (!value) co_return;
-                    auto pathOpt = std::move(value).unwrapOr(std::nullopt);
-                    if (!pathOpt.has_value()) co_return;
+                        auto path = std::move(pathOpt).value();
+                        std::error_code ec;
+                        if (!std::filesystem::exists(path, ec)) return;
 
-                    auto path = std::move(pathOpt).value();
-                    std::error_code ec;
-                    if (!std::filesystem::exists(path, ec)) co_return;
+                        gui::Engine::queueAfterDrawing([this, path = std::move(path)] {
+                            auto res = geode::utils::file::readFromJson<labels::LabelSettings>(path);
+                            if (!res) {
+                                return Popup::create(
+                                    i18n::get_("labels.import-failed"),
+                                    i18n::get_("labels.import-failed.msg")
+                                );
+                            }
 
-                    gui::Engine::queueAfterDrawing([this, path = std::move(path)] {
-                        auto res = geode::utils::file::readFromJson<labels::LabelSettings>(path);
-                        if (!res) {
-                            return Popup::create(
-                                i18n::get_("labels.import-failed"),
-                                i18n::get_("labels.import-failed.msg")
-                            );
-                        }
-
-                        s_labels.emplace_back(std::move(res).unwrap());
-                        config::set("labels", s_labels);
-                        updateLabels(true);
-                        createLabelComponent();
-                    });
-                });
+                            s_labels.emplace_back(std::move(res).unwrap());
+                            config::set("labels", s_labels);
+                            updateLabels(true);
+                            createLabelComponent();
+                        });
+                    }
+                );
             });
             tab->addButton("labels.add-new")->callback([this] {
                 gui::Engine::queueAfterDrawing([this] {
