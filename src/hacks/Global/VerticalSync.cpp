db6aea37d32c6716f650744a2541d7581eefa597
prevter <prevter@gmail.com>
2026-03-31 19:31:47 +0300

    vsync toggle macos

--- src/hacks/Global/VerticalSync.cpp
+++ src/hacks/Global/VerticalSync.cpp
@@ -3,19 +3,48 @@
 #include <modules/gui/components/toggle.hpp>
 #include <modules/hack/hack.hpp>
 
-#ifdef GEODE_IS_WINDOWS
+#ifdef GEODE_IS_DESKTOP
+
 namespace eclipse::hacks::Global {
 
     class $hack(VerticalSync) {
+        #ifdef GEODE_IS_MACOS
+        static void toggleSDLVSync(bool enabled) {
+            static auto sdl = geode::Loader::get()->getInstalledMod("zmx.sdl");
+            if (!sdl || !sdl->isLoaded()) return;
+
+            sdl->setSettingValue("disable-vsync", !enabled);
+        }
+
+        void lateInit() override {
+            auto sdl = geode::Loader::get()->getInstalledMod("zmx.sdl");
+            if (!sdl || !sdl->isLoaded()) return;
+
+            config::setTemp("global.vsync", !sdl->getSettingValue<bool>("disable-vsync"));
+            geode::listenForSettingChanges<bool>("disable-vsync", [](bool enabled) {
+                config::setTemp("global.vsync", !enabled);
+            }, sdl);
+        }
+        #endif
+
         void init() override {
+            #ifdef GEODE_IS_MACOS
+            auto sdl = geode::Loader::get()->getInstalledMod("zmx.sdl");
+            if (!sdl || !sdl->shouldLoad()) return;
+            #endif
+
             auto tab = gui::MenuTab::find("tab.global");
 
             tab->addToggle("global.vsync")
                ->handleKeybinds()
                ->setDescription()
                ->callback([](bool v) {
+                #ifdef GEODE_IS_WINDOWS
                    utils::get<GameManager>()->setGameVariable(GameVar::VerticalSync, v);
                    utils::get<AppDelegate>()->toggleVerticalSync(v);
+                #else
+                   toggleSDLVSync(v);
+                #endif
                })
                ->disableSaving();
         }
