e7fc6ff8d0900b80140973dd181f716d54afe4a5
MarioMastr <84964352+MarioMastr@users.noreply.github.com>
2026-06-01 03:06:59 +0300

    Merge pull request #417 from MarioMastr/no-do-not-flip
    Add No Do Not Flip hack

---
+++ src/hacks/Level/NoDoNotFlip.cpp
@@ -0,0 +1,48 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/EndLevelLayer.hpp>
+
+namespace eclipse::hacks::Level {
+    class $hack(NoDoNotFlip) {
+        void init() override {
+            auto tab = gui::MenuTab::find("tab.level");
+            tab->addToggle("level.nodonotflip")->setDescription()->handleKeybinds();
+        }
+
+        [[nodiscard]] const char* getId() const override { return "No Do Not Flip"; }
+    };
+
+    REGISTER_HACK(NoDoNotFlip)
+
+    class $modify(NoDoNotFlipPLHook, PlayLayer) {
+        ADD_HOOKS_DELEGATE("level.nodonotflip")
+
+		void setupHasCompleted() {
+			PlayLayer::setupHasCompleted();
+
+            if (m_doNot)
+			    m_attemptLabel->setScaleY(1.0);
+		}
+    };
+
+    class $modify(NoDoNotFlipELLHook, EndLevelLayer) {
+        ADD_HOOKS_DELEGATE("level.nodonotflip")
+
+        void customSetup() {
+            EndLevelLayer::customSetup();
+
+            if (m_playLayer->m_doNot) {
+                cocos2d::CCSprite *levelCompleteText = m_mainLayer->getChildByType<cocos2d::CCSprite>(2);
+
+                if (levelCompleteText)
+                    levelCompleteText->setFlipX(false);
+                else
+                    geode::log::warn("Failed to find level-complete-text.");
+            }
+        }
+    };
+}
